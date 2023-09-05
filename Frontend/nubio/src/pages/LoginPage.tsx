c47ab226c583d34904b9836873cf33efc3a53e65
윤태웅 <omygog@naver.com>
2023-09-05 17:58:55 +0900

    feat(LoginPage) : #5 - 로그인페이지 구성
    로그인 페이지 생성, Input생성, 버튼 생성
    
    Related to: #5

--- Frontend/nubio/src/pages/LoginPage.tsx
+++ Frontend/nubio/src/pages/LoginPage.tsx
@@ -1,1 +1,17 @@
-export {};
+import { LoginPageWrapper, LoginForm } from "../styles/SLoginPage";
+
+const LoginPage = () => {
+    return(
+        <LoginPageWrapper>
+            <img src="https://github.com/Nubi0/Nubio/assets/110621233/dd0c6a66-ca3c-4e4a-9406-89248326a3e5" alt="" />
+            <LoginForm>
+                <input type="text" name="id" />
+                <input type="text" name="pw" />
+                <button>로그인</button>
+                <button id="text">회원이 아니신가요?</button>
+            </LoginForm>
+        </LoginPageWrapper>
+    );
+};
+
+export default LoginPage;
--- Frontend/nubio/src/styles/SLoginPage.tsx
+++ Frontend/nubio/src/styles/SLoginPage.tsx
@@ -1,1 +1,30 @@
-export {};
+import styled from 'styled-components';
+
+// 로그인 페이지
+export const LoginPageWrapper = styled.div`
+    margin: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img{
+        width: 20rem;
+        height: 20rem;
+    }
+`;
+
+export const LoginForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    margin-top: 2rem;
+    input{
+        width: 15.5rem;
+        height: 2rem;
+    }
+    button{
+    }
+    #text{
+        border: none;
+        background-color: transparent;
+        color: rgba(0, 0, 0, 0.46)
+    }
+`;

6b660fc7334e78e9e1f08a8afa9080b6b48a2473
김윤욱 <reznov03048@gmail.com>
2023-09-06 15:21:19 +0900

    feat(ErrorCode) : #8 - ErrorCode enum 생성
    - ErrorCode enum 생성
    - Member의 Email 타입 검증 실패 ErrorCode 추가
    
    Related to: #8

---
+++ backend/authentication-service/src/main/java/com/authenticationservice/global/error/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.authenticationservice.global.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    // 회원
+    Invalid_Email_Format(HttpStatus.BAD_REQUEST, "M-001", "이메일 형식이 잘못됐습니다.")
+    ;
+
+    ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
+        this.httpStatus = httpStatus;
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+    private HttpStatus httpStatus;
+    private String errorCode;
+    private String message;
+}

409b74fa25ba856957ea670216c4f0656876c606
jyj143 <jyj1143@gmail.com>
2023-09-07 16:02:50 +0900

    feat(safe-service) : #15 - Create AddressFormatException
    - InvalidAddressFormatException
    - INVALID_ADDRESS 에러 코드 추가
    
    Related to: #15

--- backend/safe-service/src/main/java/com/safeservice/global/error/ErrorCode.java
+++ backend/safe-service/src/main/java/com/safeservice/global/error/ErrorCode.java
@@ -6,7 +6,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    TEST_EXCEPTION(HttpStatus.UNAUTHORIZED, "TEST-000", "에러 테스트");
+    TEST_EXCEPTION(HttpStatus.UNAUTHORIZED, "TEST-000", "에러 테스트"),
+
+    // 안전벨
+    INVALID_ADDRESS(HttpStatus.BAD_REQUEST, "SB-001", "주소 길이가 잘못되었습니다."),
+    ;
+
     ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
         this.httpStatus = httpStatus;
         this.errorCode = errorCode;
---
+++ backend/safe-service/src/main/java/com/safeservice/global/error/exception/ValidationException.java
@@ -0,0 +1,10 @@
+package com.safeservice.global.error.exception;
+
+import com.safeservice.global.error.ErrorCode;
+
+public class ValidationException extends BusinessException {
+
+    public ValidationException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}

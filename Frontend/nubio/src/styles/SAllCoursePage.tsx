6e24d38848742787e2293a473b11ecfcd6fc1e3b
윤태웅 <omygog@naver.com>
2023-09-08 00:21:37 +0900

    design(AllCoursePage) : #20 - AllCoursePage CSS 수정
    Footer에 가리는 부분 수정
    
    Closes: #20

--- Frontend/nubio/src/styles/SAllCoursePage.tsx
+++ Frontend/nubio/src/styles/SAllCoursePage.tsx
@@ -1,116 +1,119 @@
 import styled from 'styled-components';
 
 export const AllCoursePageWrapper = styled.div`
-    margin: 0 1rem;
+  margin: 0 1rem;
+  display: flex;
+  flex-direction: column;
 `;
 
-export const AllCourseWrapper = styled.div``;
+export const AllCourseWrapper = styled.div`
+  padding-bottom: 5.4rem;
+`;
 
 export const AllCourseHeaderWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const FirstLine = styled.div`
-    display: flex;
+  display: flex;
 `;
 
 export const SecondLine = styled.div`
-    font-family: 'NanumSquareExtraBold';
-    font-weight: bold;
-    font-size: 1.5rem;
-    span:first-child{
-        color: #FFC542;
-    }
+  font-family: 'NanumSquareExtraBold';
+  font-weight: bold;
+  font-size: 1.5rem;
+  span:first-child {
+    color: #ffc542;
+  }
 `;
-
 
 export const LeftSide = styled.div``;
 
 export const RightSide = styled.div``;
 
 export const CategoryWrapper = styled.div`
-    display: flex;
-    .active{
-        color: #FFC542;
-    }
+  display: flex;
+  .active {
+    color: #ffc542;
+  }
 `;
 
 export const AllCourseListWrapper = styled.div`
-    margin-top: 1rem;
+  margin-top: 1rem;
 `;
 
 export const AllCourseItemWrapper = styled.div`
-    display: flex;
-    overflow-x: hidden;
-    margin-top: 1.5rem;
+  display: flex;
+  overflow-x: scroll;
+  margin-top: 1.5rem;
 `;
 
 export const BigImg = styled.img`
-    width: 6.75rem;
-    height: 6.5rem;
-    border-radius: 0.5rem;
+  width: 6.75rem;
+  height: 6.5rem;
+  border-radius: 0.5rem;
 `;
 
 export const DetailWrapper = styled.div`
-    margin-left: 1rem;
+  margin-left: 1rem;
 `;
 
 export const HeadContent = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 `;
 
 export const Title = styled.div`
-    font-family: 'NanumSquareExtraBold';
-    font-weight: bold;
+  font-family: 'NanumSquareExtraBold';
+  font-weight: bold;
 `;
 
 export const CourseTag = styled.div`
-    border: 0.5px solid #F9373F;
-    border-radius: 10px;
-    font-size: 0.5rem;
-    width: 2rem;
-    height: 1rem;
-    color: #F9373F;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-left: 0.25rem;
+  border: 0.5px solid #f9373f;
+  border-radius: 10px;
+  font-size: 0.5rem;
+  width: 2rem;
+  height: 1rem;
+  color: #f9373f;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 0.25rem;
 `;
 
 export const FavImg = styled.img`
-    margin-left: 0.25rem;
+  margin-left: 0.25rem;
 `;
 
 export const PlaceItem = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-right: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 0.5rem;
 `;
 
 export const PlaceImg = styled.img`
-    width: 3.5rem;
-    height: 3.25rem;
-    border-radius: 0.5rem;
+  width: 3.5rem;
+  height: 3.25rem;
+  border-radius: 0.5rem;
 `;
 
 export const PlaceName = styled.div`
-    font-size: 0.5rem;
+  font-size: 0.5rem;
 `;
 
 export const PlaceListWrapper = styled.div`
-    display: flex;
-    margin-top: 0.5rem;
+  display: flex;
+  margin-top: 0.5rem;
 `;
 
 export const EnjoyHeaderWrapper = styled.div`
-    margin: 1rem 0;
-    display: flex;
-    align-items: center;
-    font-size: 1.5rem;
-    div{
-        margin-left: 1rem;
-    }
+  margin: 1rem 0;
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+  div {
+    margin-left: 1rem;
+  }
 `;

9f63a6acd1cde7a8cb1473a98be6f335686b0301
김윤욱 <reznov03048@gmail.com>
2023-09-07 12:01:54 +0900

    feat(DuplicateMemberException) : #14 - Email이 중복되는 Member가 회원가입 할 때 예외 구현
    Email이 중복되는 Member가 회원가입 할 때 예외 구현
    
    Related to: #14

--- backend/authentication-service/src/main/java/com/authenticationservice/global/error/ErrorCode.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/global/error/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     INVALID_OAUTHTYPE(HttpStatus.BAD_REQUEST, "M-002", "로그인 타입이 잘못됐습니다."),
     INVALID_ROLE(HttpStatus.BAD_REQUEST, "M-003", "회원 역할 타입이 잘못됐습니다."),
     INVALID_GENDER(HttpStatus.BAD_REQUEST, "M-004", "성별 타입이 잘못됐습니다."),
-    INVALID_BIRTH_FORMAT(HttpStatus.BAD_REQUEST, "M-005", "생년월일 형식이 잘못됐습니다.")
+    INVALID_BIRTH_FORMAT(HttpStatus.BAD_REQUEST, "M-005", "생년월일 형식이 잘못됐습니다."),
+    DUPLICATE_MEMBER_EXIST(HttpStatus.BAD_REQUEST, "M-006", "중복된 회원가입 입니다.")
     ;
 
     ErrorCode(HttpStatus httpStatus, String errorCode, String message) {

afee234d55414c96d513f34bed4a24dd38e5c8d4
김윤욱 <reznov03048@gmail.com>
2023-09-07 12:03:35 +0900

    feat(MemberService) : #14 - MemberService interface 생성
    - 회원가입 기능
    
    Related to: #14

---
+++ backend/authentication-service/src/main/java/com/authenticationservice/domain/member/service/MemberService.java
@@ -0,0 +1,8 @@
+package com.authenticationservice.domain.member.service;
+
+import com.authenticationservice.domain.member.entity.Member;
+
+public interface MemberService {
+
+    Member register(Member member);
+}

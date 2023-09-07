90a3f4c2f08f49efd17c5bc5d4babf8c76326607
jyj143 <jyj1143@gmail.com>
2023-09-07 10:25:33 +0900

    feat(safe-service) : #17 -  Create Exception feature
    - BusinessException
    - ErrorCode
    - ErrorResponse
    - GlobalExceptionHandler
    
    Related to: #17

---
+++ backend/safe-service/src/main/java/com/safeservice/global/error/exception/BusinessException.java
@@ -0,0 +1,14 @@
+package com.safeservice.global.error.exception;
+
+import com.safeservice.global.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}

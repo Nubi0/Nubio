0e0697b013c6abd975574cff77e56b383f136119
이서현 <kathyleesh7@gmail.com>
2023-09-08 17:33:59 +0900

    feat(WebConfig) : #40 - cors 설정 구현
    - origin, method, header 관련 설정 추가
    
    Related to: #40

---
+++ backend/authentication-service/src/main/java/com/authenticationservice/global/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.authenticationservice.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig {
+
+    //CORS 설정
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));
+        configuration.setAllowCredentials(true); // 허용된 도메인에 쿠키를 전송하도록 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}

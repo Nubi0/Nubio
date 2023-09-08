99b2df08f1dc813b352c73355d5dd129e003dac5
윤태웅 <omygog@naver.com>
2023-09-08 17:19:57 +0900

    feat(CouresDetail) : #39 - CourseDetail 컴포넌트 생성
    CourseDetail 컴포넌트 생성 완료
    
    Related to: #39

---
+++ Frontend/nubio/src/components/enjoyHome/CourseDetail/CourseDetail.tsx
@@ -0,0 +1,13 @@
+import { CourseDetailWrapper } from "../../../styles/SCourseDeatilPage";
+import CourseDetailList from "./CoursDetailList/CourseDetailList";
+
+const CourseDetail = () => {
+    return(
+        <CourseDetailWrapper>
+            <CourseDetailList />
+        </CourseDetailWrapper>
+
+    );
+};
+
+export default CourseDetail;
--- Frontend/nubio/src/styles/SCourseDeatilPage.tsx
+++ Frontend/nubio/src/styles/SCourseDeatilPage.tsx
@@ -1,1 +1,111 @@
-export {};
+import styled from 'styled-components';
+
+export const CourseDetailPageWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const CourseDetailWrapper = styled.div`
+    margin: 0 1rem;
+    padding-bottom: 5.4rem;
+    position: relative;
+    background-color: red;
+
+`;
+
+export const CourseDetailListWrapper = styled.div`
+    position: absolute;
+    bottom: 5.4rem;
+    width: 100%;
+    border-radius: 30px;
+    background-color: red;
+`;
+
+export const DetailHr = styled.hr`
+    width: 50%;
+    height: 3px;
+    border-radius: 10px;
+    background-color: gray;
+`;
+
+export const ItemWrapper = styled.div`
+    display: flex;
+    overflow-x: scroll;
+`;
+
+export const CourseDetailItemWrapper = styled.div`
+    background-color: white;
+    color: white;
+    position: relative;
+    width: 12.5rem;
+    height: 14.25rem;
+    border-radius: 1rem 1rem 0 0;
+    margin-left: 2.5rem;
+`;
+
+export const PlaceImg = styled.img`
+    width: 12.5rem;
+    height: 11rem;
+    border-radius: 1rem 1rem 0 0;
+`;
+
+export const InfoWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    position: absolute;
+    bottom: 23%;
+    left: 0%;
+    width: 100%;
+    background: linear-gradient(to bottom, transparent, rgba(0, 0, 0, 1));
+`;
+
+export const Left = styled.div`
+    font-size: 2.5rem;
+`;
+
+export const Right = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const CategoryName = styled.div`
+    font-size: 0.5rem;
+`;
+
+export const PlaceName = styled.div`
+    font-size: 1rem;
+    font-weight: 800;
+`;
+
+export const Address = styled.div`
+    font-size: 0.5rem;
+    display: flex;
+`;
+
+
+export const CourseButtonWrapper = styled.div`
+    background-color: #FFC542;
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem 2rem;
+    div{
+        display: flex;
+        background-color: transparent;
+        align-items: center;
+    }
+    button {
+        background-color: transparent;
+        border: none;
+        color: white;
+        margin-left: 0.5rem;
+        font-size: inherit;
+        padding: 0;
+    }
+`;
+
+export const ButtonDivider = styled.hr`
+    border: none; 
+    width: 0.25rem;
+    background-color: white;
+    margin: 0 1rem;
+`;

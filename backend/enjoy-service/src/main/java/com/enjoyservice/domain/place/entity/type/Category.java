1163a30304e74ec0ae894a85b25e021c12fdddb3
김윤욱 <reznov03048@gmail.com>
2023-09-11 15:12:26 +0900

    feat(Category) : #54 - Category 생성
    Category 생성
    
    Related to: #54

---
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/place/entity/type/Category.java
@@ -0,0 +1,34 @@
+package com.enjoyservice.domain.place.entity.type;
+
+import com.enjoyservice.domain.place.entity.constant.GroupCode;
+import com.enjoyservice.domain.place.entity.constant.GroupName;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category_group_code")
+    private GroupCode groupCode;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category_group_name")
+    private GroupName groupName;
+
+    @Embedded
+    private Detail detail;
+
+    private Category(GroupCode groupCode, GroupName groupName, Detail detail) {
+        this.groupCode = groupCode;
+        this.groupName = groupName;
+        this.detail = detail;
+    }
+
+    public static Category from(final GroupCode groupCode, final GroupName groupName, final Detail detail) {
+        // TODO: 각 카테고리별 짝이 맞는지 검증하는 validate() 넣어야 됨
+        return new Category(groupCode, groupName, detail);
+    }
+}

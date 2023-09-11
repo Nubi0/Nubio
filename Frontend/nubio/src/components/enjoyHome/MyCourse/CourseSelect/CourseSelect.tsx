ed2c3868fe85673bfd84cbaff96ad57a02501d05
윤태웅 <omygog@naver.com>
2023-09-12 02:37:03 +0900

    feat(CourseSelect) : #62 - 코스 거리 계산 기능
    코스를 그리면 거리를 계산해주는 기능 추가
    
    Related to: #62

--- Frontend/nubio/src/components/enjoyHome/MyCourse/CourseSelect/CourseSelect.tsx
+++ Frontend/nubio/src/components/enjoyHome/MyCourse/CourseSelect/CourseSelect.tsx
@@ -1,92 +1,157 @@
-import { useState, useRef } from 'react';
-import { CourseSelectWrapper, MapWrapper } from "../../../../styles/SCourseSelectPage";
-import { Map, DrawingManager } from 'react-kakao-maps-sdk';
+import React, { useEffect, useState } from 'react';
+import { CourseSelectWrapper } from '../../../../styles/SCourseSelectPage';
+import { Map, Polyline, CustomOverlayMap } from 'react-kakao-maps-sdk';
 
-const CourseSelect =() => {
-    type DrawingManagerType = kakao.maps.drawing.DrawingManager<kakao.maps.drawing.OverlayType.POLYLINE>
-    const managerRef = useRef<DrawingManagerType>(null)
+const CourseSelect = () => {
+  const [isdrawing, setIsDrawing] = useState(false);
+  const [clickLine, setClickLine] = useState<any>(null);
+  const [paths, setPaths] = useState<any[]>([]);
+  const [distances, setDistances] = useState<number[]>([]);
+  const [mousePosition, setMousePosition] = useState({
+    lat: 0,
+    lng: 0,
+  });
+  const [moveLine, setMoveLine] = useState<any>(null);
 
-    const [overlayData, setOverlayData] = useState<
-    ReturnType<DrawingManagerType["getData"]>
-  >({
-    arrow: [],
-    circle: [],
-    ellipse: [],
-    marker: [],
-    polygon: [],
-    polyline: [],
-    rectangle: [],
-  })
-
-  function selectOverlay(
-    type: string)  {
-    const manager = managerRef.current
-    if(manager){
-        manager.cancel()
-        manager.select(type)
+  const handleClick = (_map: any, mouseEvent: any) => {
+    if (!isdrawing) {
+      setDistances([]);
+      setPaths([]);
     }
-  }
+    setPaths((prev) => [
+      ...prev,
+      {
+        lat: mouseEvent.latLng.getLat(),
+        lng: mouseEvent.latLng.getLng(),
+      },
+    ]);
+    setDistances((prev) => [
+      ...prev,
+      Math.round((clickLine?.getLength() || 0) + (moveLine?.getLength() || 0)),
+    ]);
+    setIsDrawing(true);
+  };
 
-  function drawOverlayData() {
-    const manager = managerRef.current
-    if(manager){
-        setOverlayData(manager.getData())
-    }
-  }
+  const handleMouseMove = (_map: any, mouseEvent: any) => {
+    setMousePosition({
+      lat: mouseEvent.latLng.getLat(),
+      lng: mouseEvent.latLng.getLng(),
+    });
+  };
 
-  // Drawing Manager에서 가져온 데이터 중
-  // 선과 다각형의 꼭지점 정보를 latlng 배열로 반환하는 함수입니다
-  function pointsToPath(points: Array<{ x: number; y: number }>) {
-    return points.map((point) => ({
-      lat: point.y,
-      lng: point.x,
-    }))
-  }
-    return(
-        <CourseSelectWrapper>
-            <Map
-        center={{
-          // 지도의 중심좌표
-          lat: 33.450701,
-          lng: 126.570667,
-        }}
-        style={{
-          width: "100%",
-          height: "450px",
-        }}
-        level={3} // 지도의 확대 레벨
-      >
-        <DrawingManager
-          ref={managerRef}
-          drawingMode={['polyline']}
-          guideTooltip={["draw", "drag", "edit"]}
-          polylineOptions={{
-            // 선 옵션입니다
-            draggable: true, // 그린 후 드래그가 가능하도록 설정합니다
-            removable: true, // 그린 후 삭제 할 수 있도록 x 버튼이 표시됩니다
-            editable: true, // 그린 후 수정할 수 있도록 설정합니다
-            strokeColor: "#39f", // 선 색
-            hintStrokeStyle: "dash", // 그리중 마우스를 따라다니는 보조선의 선 스타일
-            hintStrokeOpacity: 0.5, // 그리중 마우스를 따라다니는 보조선의 투명도
-          }}
-        />
-      </Map>
-      <div
-        style={{
-          display: "flex",
-          gap: "8px",
-        }}
-      >
-        <button
-          onClick={(e) => {
-            selectOverlay('polyline')
-          }}
-        >
-          선
-        </button>
-        </div>
-        </CourseSelectWrapper>
+  const handleRightClick = (_map: any, _mouseEvent: any) => {
+    setIsDrawing(false);
+  };
+
+  const DistanceInfo = ({ distance }: any) => {
+    const walkTime = Math.floor(distance / 67);
+    const bicycleTime = Math.floor(distance / 227);
+
+    return (
+      <ul className="dotOverlay distanceInfo">
+        <li>
+          <span className="label">총거리</span> <span className="number">{distance}</span>{' '}
+          m
+        </li>
+        <li>
+          <span className="label">도보</span>{' '}
+          {walkTime > 60 ? (
+            <>
+              <span className="number">{Math.floor(walkTime / 60)}</span> 시간{' '}
+            </>
+          ) : null}
+          <span className="number">{walkTime % 60}</span> 분
+        </li>
+        <li>
+          <span className="label">자전거</span>{' '}
+          {bicycleTime > 60 ? (
+            <>
+              <span className="number">{Math.floor(bicycleTime / 60)}</span> 시간{' '}
+            </>
+          ) : null}
+          <span className="number">{bicycleTime % 60}</span> 분
+        </li>
+      </ul>
     );
+  };
+
+  return (
+    <>
+      <CourseSelectWrapper>
+        <Map
+          id={`map`}
+          center={{
+            lat: 37.498004414546934,
+            lng: 127.02770621963765,
+          }}
+          style={{
+            width: '100%',
+            height: '450px',
+          }}
+          level={3}
+          onClick={handleClick}
+          onRightClick={handleRightClick}
+          onMouseMove={handleMouseMove}
+        >
+          <Polyline
+            path={paths}
+            strokeWeight={3}
+            strokeColor={'#db4040'}
+            strokeOpacity={1}
+            strokeStyle={'solid'}
+            onCreate={(polyline) => setClickLine(polyline)}
+          />
+          {paths.map((path, index) => (
+            <CustomOverlayMap
+              key={`dot-${path.lat},${path.lng}`}
+              position={path}
+              zIndex={1}
+            >
+              <span className="dot"></span>
+            </CustomOverlayMap>
+          ))}
+          {paths.length > 1 &&
+            distances.slice(1).map((distance, index) => (
+              <CustomOverlayMap
+                key={`distance-${paths[index + 1].lat},${paths[index + 1].lng}`}
+                position={paths[index + 1]}
+                yAnchor={1}
+                zIndex={2}
+              >
+                {!isdrawing && distances.length === index + 2 ? (
+                  <DistanceInfo distance={distance} />
+                ) : (
+                  <div className="dotOverlay">
+                    거리 <span className="number">{distance}</span> m
+                  </div>
+                )}
+              </CustomOverlayMap>
+            ))}
+          <Polyline
+            path={isdrawing ? [paths[paths.length - 1], mousePosition] : []}
+            strokeWeight={3}
+            strokeColor={'#db4040'}
+            strokeOpacity={0.5}
+            strokeStyle={'solid'}
+            onCreate={(polyline) => setMoveLine(polyline)}
+          />
+          {isdrawing && (
+            <CustomOverlayMap position={mousePosition} yAnchor={1} zIndex={2}>
+              <div className="dotOverlay distanceInfo">
+                총거리{' '}
+                <span className="number">
+                  {Math.round(
+                    (clickLine?.getLength() || 0) + (moveLine?.getLength() || 0)
+                  )}
+                </span>{' '}
+                m
+              </div>
+            </CustomOverlayMap>
+          )}
+        </Map>
+      </CourseSelectWrapper>
+    </>
+  );
 };
 
 export default CourseSelect;

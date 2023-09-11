c874d2c59f95aebf26c5a49f470c6a40ea40c5a9
김민규 <rrrr3321@gmail.com>
2023-09-11 16:59:58 +0900

    feat(ReportController) : #61 - ReportController 구현
    - 생성, 수정, 조회 api 구현
    
    Related to: #61

--- backend/safe-service/src/main/java/com/safeservice/api/report/controller/ReportController.java
+++ backend/safe-service/src/main/java/com/safeservice/api/report/controller/ReportController.java
@@ -2,9 +2,13 @@ package com.safeservice.api.report.controller;
 
 import com.safeservice.api.ApiResponse;
 import com.safeservice.api.report.dto.ReportRequestDto;
+import com.safeservice.api.report.dto.ReportListDto;
+import com.safeservice.api.report.dto.ReportResponseDto;
+import com.safeservice.api.report.dto.ReportUpdateRequestDto;
 import com.safeservice.api.report.service.ReportInfoService;
 import com.safeservice.global.resolver.identification.Identification;
 import com.safeservice.global.resolver.identification.IdentificationDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,8 +25,23 @@ public class ReportController {
     @PostMapping("/report")
     public ApiResponse<String> createReport(@Identification IdentificationDto identificationDto,
                                             @RequestPart("file") List<MultipartFile> files,
-                                            @RequestPart("report") ReportRequestDto reportRequestDto) {
+                                            @Valid  @RequestPart("report") ReportRequestDto reportRequestDto) {
         reportInfoService.createReport(reportRequestDto, files, identificationDto.getIdentification());
         return ApiResponse.ok("생성 완료");
     }
+
+    @PutMapping("/report")
+    public ApiResponse<String> updateReport(@Identification IdentificationDto identificationDto,
+                                            @RequestPart("file") List<MultipartFile> files,
+                                            @Valid @RequestPart("report") ReportUpdateRequestDto reportUpdateRequestDto) {
+        reportInfoService.updateReport(reportUpdateRequestDto, files, identificationDto.getIdentification());
+        return ApiResponse.ok("수정 완료");
+    }
+
+    @GetMapping("/report")
+    public ApiResponse<ReportResponseDto> searchAll(@Identification IdentificationDto identificationDto) {
+        ReportResponseDto responseReport = reportInfoService.searchAll(identificationDto.getIdentification());
+        return ApiResponse.ok(responseReport);
+    }
+
 }

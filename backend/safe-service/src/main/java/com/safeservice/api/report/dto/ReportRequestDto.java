d159f2b12e141b03abfcbb8fa78e29708d32a6e0
김민규 <rrrr3321@gmail.com>
2023-09-11 16:58:57 +0900

    feat(ReportDto) : #61 - Report Controller Dto 생성
    - Report 생성 ReportRequestDto 생성
    - Report 조회 ReportResponseDto, ReportFileDto 생성
    
    Related to: #61

--- backend/safe-service/src/main/java/com/safeservice/api/report/dto/ReportRequestDto.java
+++ backend/safe-service/src/main/java/com/safeservice/api/report/dto/ReportRequestDto.java
@@ -4,11 +4,16 @@ import com.safeservice.domain.report.entity.Report;
 import com.safeservice.domain.report.entity.constant.report.ReportType;
 import com.safeservice.domain.report.entity.type.report.Active;
 import com.safeservice.domain.report.entity.type.report.Content;
+import com.safeservice.domain.report.entity.type.report.Position;
 import com.safeservice.domain.report.entity.type.report.Title;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -19,15 +24,30 @@ import java.util.List;
 @NoArgsConstructor
 public class ReportRequestDto {
 
+    @Length(max = 200, message = "댓글은 200자 이하여야 합니다.")
+    @NotEmpty(message = "제목은 필수 값 입니다.")
     private String title;
+
+    @NotEmpty(message = "내용은 필수 값 입니다.")
     private String content;
+
+    @NotEmpty(message = "제보 타입은 필수 값 입니다.")
     private String reportType;
 
-    public static ReportRequestDto of(String title, String content, String reportType) {
+    @NotEmpty(message = "위도는 필수 값 입니다.")
+    private double latitude;
+
+    @NotEmpty(message = "경도는 필수 값 입니다.")
+    private double longitude;
+
+
+    public static ReportRequestDto of(String title, String content, String reportType, double latitude, double longitude) {
         return ReportRequestDto.builder()
                 .title(title)
                 .content(content)
-                .reportType(reportType).build();
+                .reportType(reportType)
+                .latitude(latitude)
+                .longitude(longitude).build();
     }
 
     public static Report toEntity(ReportRequestDto reportRequestDto, String identification) {
@@ -35,6 +55,7 @@ public class ReportRequestDto {
                 .title(Title.from(reportRequestDto.getTitle()))
                 .content(Content.from(reportRequestDto.getContent()))
                 .reportType(ReportType.from(reportRequestDto.getReportType()))
+                .position(Position.of(reportRequestDto.getLongitude(), reportRequestDto.getLatitude()))
                 .active(Active.from(true))
                 .identification(identification).build();
     }
---
+++ backend/safe-service/src/main/java/com/safeservice/api/report/dto/ReportUpdateRequestDto.java
@@ -0,0 +1,51 @@
+package com.safeservice.api.report.dto;
+
+import com.safeservice.domain.report.entity.Report;
+import com.safeservice.domain.report.entity.constant.report.ReportType;
+import com.safeservice.domain.report.entity.type.report.Active;
+import com.safeservice.domain.report.entity.type.report.Content;
+import com.safeservice.domain.report.entity.type.report.Position;
+import com.safeservice.domain.report.entity.type.report.Title;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportUpdateRequestDto {
+
+    @NotBlank(message = "제보Id는 필수 값 입니다.")
+    private Long reportId;
+
+    @Length(max = 200, message = "댓글은 200자 이하여야 합니다.")
+    @NotBlank(message = "제목은 필수 값 입니다.")
+    private String title;
+
+    @NotBlank(message = "내용은 필수 값 입니다.")
+    private String content;
+
+    @NotBlank(message = "제보 타입은 필수 값 입니다.")
+    private String reportType;
+
+    @NotBlank(message = "위도는 필수 값 입니다.")
+    private double latitude;
+
+    @NotBlank(message = "경도는 필수 값 입니다.")
+    private double longitude;
+
+
+    public static Report toEntity(ReportUpdateRequestDto reportUpdateRequestDto) {
+        return Report.builder()
+                .title(Title.from(reportUpdateRequestDto.getTitle()))
+                .content(Content.from(reportUpdateRequestDto.getContent()))
+                .reportType(ReportType.from(reportUpdateRequestDto.getReportType()))
+                .position(Position.of(reportUpdateRequestDto.getLongitude(), reportUpdateRequestDto.getLatitude()))
+                .active(Active.from(true)).build();
+    }
+}

21085305967e1c99d3b920edc6210ea9ac48873f
김윤욱 <reznov03048@gmail.com>
2023-09-11 14:38:12 +0900

    test(GroupNameTest) : #54 - GroupName이 맞으면 GroupName 객체를 생성하는 test 작성
    GroupName이 맞으면 GroupName 객체를 생성하는 test 작성
    
    Related to: #54

--- backend/enjoy-service/src/test/java/com/enjoyservice/domain/place/entity/constant/GroupNameTest.java
+++ backend/enjoy-service/src/test/java/com/enjoyservice/domain/place/entity/constant/GroupNameTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -22,6 +23,16 @@ class GroupNameTest {
     @ValueSource(strings = {"AB2", "P6", "7", "QWER"})
     void isNotGroupCode(String input) {
         assertFalse(GroupName.isGroupName(input));
+    }
+
+    @DisplayName("GroupName이 맞으면 GroupName 객체를 생성한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"편의점", "주차장", "문화시설", "관광명소", "숙박", "음식점", "카페"})
+    void from(String input) {
+        // when
+        GroupName groupName = GroupName.from(input);
+        // then
+        assertThat(groupName).isEqualTo(GroupName.valueOf(input));
     }
 
 }

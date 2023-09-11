d59ff10f08fb2e7d585960ac26382ef8d2471b6b
김윤욱 <reznov03048@gmail.com>
2023-09-11 14:40:46 +0900

    test(GroupNameTest) : #54 - GroupName이 아니면 예외를 발생시키는 test 작성
    GroupName이 아니면 예외를 발생시키는 test 작성
    
    Related to: #54

--- backend/enjoy-service/src/test/java/com/enjoyservice/domain/place/entity/constant/GroupNameTest.java
+++ backend/enjoy-service/src/test/java/com/enjoyservice/domain/place/entity/constant/GroupNameTest.java
@@ -1,11 +1,13 @@
 package com.enjoyservice.domain.place.entity.constant;
 
+import com.enjoyservice.domain.place.exception.InvalidGroupNameException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -14,14 +16,14 @@ class GroupNameTest {
     @DisplayName("GroupName이 맞으면 true를 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"편의점", "주차장", "문화시설", "관광명소", "숙박", "음식점", "카페"})
-    void isGroupCode(String input) {
+    void isGroupName(String input) {
         assertTrue(GroupName.isGroupName(input));
     }
 
     @DisplayName("GroupName이 아니면 false를 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"AB2", "P6", "7", "QWER"})
-    void isNotGroupCode(String input) {
+    void isNotGroupName(String input) {
         assertFalse(GroupName.isGroupName(input));
     }
 
@@ -33,6 +35,15 @@ class GroupNameTest {
         GroupName groupName = GroupName.from(input);
         // then
         assertThat(groupName).isEqualTo(GroupName.valueOf(input));
+    }
+
+    @DisplayName("GroupName이 아니면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"AB2", "P6", "7", "QWER"})
+    void canNotFrom(String input) {
+        // when then
+        assertThatThrownBy(() -> GroupName.from(input))
+                .isInstanceOf(InvalidGroupNameException.class);
     }
 
 }

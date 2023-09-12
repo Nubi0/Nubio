c95c3d14e02f192702a5decaf434b806b19d8928
김윤욱 <reznov03048@gmail.com>
2023-09-12 10:01:30 +0900

    test(LongitudeTest) : #54 - LongitudeTest 객체 생성 test 작성
    LongitudeTest 객체 생성 test 작성
    
    Related to: #54

--- backend/enjoy-service/src/test/java/com/enjoyservice/domain/place/entity/type/LongitudeTest.java
+++ backend/enjoy-service/src/test/java/com/enjoyservice/domain/place/entity/type/LongitudeTest.java
@@ -7,14 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class LatitudeTest {
 
-    @DisplayName("주소의 경도 객체가 생성된다.")
+class LongitudeTest {
+
+    @DisplayName("주소의 위도 객체가 생성된다.")
     @Test
     void from() {
         // given
-        Double value = 126.94831126744977;
+        Double value = 37.27629056901903;
         // when then
-        assertNotNull(Latitude.from(value));
+        assertNotNull(Longitude.from(value));
     }
 }

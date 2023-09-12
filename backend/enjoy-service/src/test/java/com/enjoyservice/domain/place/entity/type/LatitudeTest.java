bbbda4bf97d97a5e7ed7fb39f54a237de5c6cf6a
김윤욱 <reznov03048@gmail.com>
2023-09-12 09:59:10 +0900

    test(LatitudeTest) : #54 - Latitude 객체 생성 test 작성
    Latitude 객체 생성 test 작성
    
    Related to: #54

---
+++ backend/enjoy-service/src/test/java/com/enjoyservice/domain/place/entity/type/LatitudeTest.java
@@ -0,0 +1,18 @@
+package com.enjoyservice.domain.place.entity.type;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LatitudeTest {
+
+    @DisplayName("주소의 경도 객체가 생성된다.")
+    @Test
+    void from() {
+        // given
+        Double value = 126.94831126744977;
+        // when then
+        assertNotNull(Latitude.from(value));
+    }
+}

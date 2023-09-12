e07afa10775de440c9551d04e366f4af62005380
jyj143 <jyj1143@gmail.com>
2023-09-12 09:13:16 +0900

    feat(config) : #55 - Add Srping data MongoDB, Spring data Jpa Transactional
    - MongoDBConfig
    - JpaConfig
    
    Related to: #55

--- backend/safe-service/src/main/java/com/safeservice/global/config/jpa/JpaConfig.java
+++ backend/safe-service/src/main/java/com/safeservice/global/config/jpa/JpaConfig.java
@@ -1,13 +1,30 @@
 package com.safeservice.global.config.jpa;
 
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaAuditing
-@EnableJpaRepositories(basePackages = "com.safeservice.domain.*.repository")
+@EnableTransactionManagement
+@RequiredArgsConstructor
+//@EnableJpaRepositories(basePackages = "com.safeservice.domain.*.repository")
 public class JpaConfig {
+
+    private final EntityManagerFactory entityManagerFactory;
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
 
 }
 
--- backend/safe-service/src/main/java/com/safeservice/global/config/mongodb/MongoDBConfig.java
+++ backend/safe-service/src/main/java/com/safeservice/global/config/mongodb/MongoDBConfig.java
@@ -12,18 +12,25 @@ import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.transaction.ChainedTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableMongoAuditing
+@EnableTransactionManagement
 @EnableMongoRepositories(basePackages = "com.safeservice.domain.*.mongo")
-public class MongoDBConfig {
+public class MongoDBConfig  implements TransactionManagementConfigurer   {
 
+    private final MongoDatabaseFactory mongoDatabaseFactory;
     private final MongoMappingContext mongoMappingContext;
+    private final PlatformTransactionManager transactionManager; // Inject the JPA transaction manager
 
     @Bean
-    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory mongoDatabaseFactory,
-                                                       MongoMappingContext mongoMappingContext) {
+    public MappingMongoConverter mappingMongoConverter() {
         DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
         MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
         converter.setTypeMapper(new DefaultMongoTypeMapper(null));
@@ -31,8 +38,12 @@ public class MongoDBConfig {
     }
 
     @Bean
-    public MongoTransactionManager transactionManager(MongoDatabaseFactory mongoDatabaseFactory) {
+    public PlatformTransactionManager mongoTransactionManager() {
         return new MongoTransactionManager(mongoDatabaseFactory);
     }
 
+    @Override
+    public TransactionManager annotationDrivenTransactionManager() {
+        return new ChainedTransactionManager(transactionManager, mongoTransactionManager());
+    }
 }

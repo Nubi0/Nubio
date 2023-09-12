b4ded0e605e90efbfa513c1ba2fcc0193d6b5934
김윤욱 <reznov03048@gmail.com>
2023-09-12 15:44:06 +0900

    feat(Course) : #54 - Course entity 생성
    Course entity 생성
    
    Related to: #54

---
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/course/entity/Course.java
@@ -0,0 +1,20 @@
+package com.enjoyservice.domain.course.entity;
+
+import com.enjoyservice.domain.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Course extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+}

c5f832defe21a432da8123500d61eada4d2f4a27
김민규 <rrrr3321@gmail.com>
2023-09-13 02:44:31 +0900

    feat(EmergencyMessage) : #66 - EmergencyMessage Entity
    - EmergencyMessage 생성
    
    Related to: #66

--- backend/safe-service/src/main/java/com/safeservice/domain/emergencymessage/entity/EmergencyMessage.java
+++ backend/safe-service/src/main/java/com/safeservice/domain/emergencymessage/entity/EmergencyMessage.java
@@ -1,23 +1,52 @@
 package com.safeservice.domain.emergencymessage.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.safeservice.domain.emergencymessage.entity.constant.EmerStage;
+import com.safeservice.domain.emergencymessage.entity.constant.EmerType;
+import com.safeservice.domain.emergencymessage.entity.type.Address;
+import com.safeservice.domain.emergencymessage.entity.type.MdId;
+import com.safeservice.domain.emergencymessage.entity.type.Message;
+import com.safeservice.domain.emergencymessage.entity.type.OccurredTime;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Builder
 public class EmergencyMessage {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Embedded
+    private MdId mdId;
 
+    @Enumerated(EnumType.STRING)
+    private EmerType emerType;
+
+    @Enumerated(EnumType.STRING)
+    private EmerStage emerStage;
+
+    @Embedded
+    private Address address;
+
+    @Embedded
+    private Message message;
+
+    @Embedded
+    private OccurredTime occurredTime;
+
+    @Builder
+    public EmergencyMessage(MdId mdId, EmerType emerType, EmerStage emerStage, Address address, Message message, OccurredTime occurredTime) {
+        this.mdId = mdId;
+        this.emerType = emerType;
+        this.emerStage = emerStage;
+        this.address = address;
+        this.message = message;
+        this.occurredTime = occurredTime;
+    }
 }

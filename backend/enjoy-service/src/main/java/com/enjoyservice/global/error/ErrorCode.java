e58cbead6c42a00cbd8c0f641288460b38ae68fd
김윤욱 <reznov03048@gmail.com>
2023-09-13 10:40:37 +0900

    feat(PlaceNotFoundException) : #69 - 없는 Place를 조회했을 때 발생시킬 예외 생성
    없는 Place를 조회했을 때 발생시킬 예외 생성
    
    Related to: #69

--- backend/enjoy-service/src/main/java/com/enjoyservice/global/error/ErrorCode.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/global/error/ErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // 장소
-    SPACE_NOT_EXISTS(HttpStatus.BAD_REQUEST, "S-001", "해당 장소는 존재하지 않습니다."),
+    PLACE_NOT_FOUND(HttpStatus.BAD_REQUEST, "S-001", "해당 장소는 존재하지 않습니다."),
     INVALID_GROUP_CODE(HttpStatus.BAD_REQUEST, "S-002", "해당 장소의 GroupCode는 존재하지 않습니다."),
     INVALID_GROUP_NAME(HttpStatus.BAD_REQUEST, "S-003", "해당 장소의 GroupName은 존재하지 않습니다."),
     INVALID_PHONE_NUMBER_FORMAT(HttpStatus.BAD_REQUEST, "S-004", "해당 PhoneNumber의 형식이 잘못되었습니다."),

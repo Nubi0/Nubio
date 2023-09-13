60e57a4e2731cb0dbec58992e06e51dcd5598af2
김윤욱 <reznov03048@gmail.com>
2023-09-13 12:57:34 +0900

    feat(PlaceLikeRepository) : #69 - PlaceLikeRepository 생성 및 Place의 좋아요 수 조회 기능 구현
    Place의 좋아요 수 조회 기능 구현
    
    Related to: #69

---
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/placelike/repository/PlaceLikeRepository.java
@@ -0,0 +1,13 @@
+package com.enjoyservice.domain.placelike.repository;
+
+import com.enjoyservice.domain.place.entity.Place;
+import com.enjoyservice.domain.placelike.entity.PlaceLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface PlaceLikeRepository extends JpaRepository<PlaceLike, Long> {
+
+    @Query("select count(pl) from PlaceLike pl where pl.place = :place")
+    int getCountByPlace(@Param("place") Place place);
+}

744b3400d0922c03cc2c26d842932ca0105c76e2
jyj143 <jyj1143@gmail.com>
2023-09-13 14:37:39 +0900

    feat(SafetyFacilityInfoService) : #70 - Use CsvToBean to map CSV records to a list of objects
    - Use CsvToBean to map CSV records to a list of objects
    
    Related to: #70

---
+++ backend/safe-service/src/main/java/com/safeservice/api/facility/service/SafetyFacilityInfoService.java
@@ -0,0 +1,62 @@
+package com.safeservice.api.facility.service;
+
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.safeservice.api.facility.dto.request.SafetyFacilityDto;
+import com.safeservice.domain.facility.constant.FacilityType;
+import com.safeservice.domain.facility.entity.SafetyFacility;
+import com.safeservice.domain.facility.service.SafetyFacilityService;
+import com.safeservice.global.error.ErrorCode;
+import com.safeservice.global.error.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.geo.Point;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class SafetyFacilityInfoService {
+
+    private final SafetyFacilityService safetyFacilityService;
+
+    public void registerSafetyBell(MultipartFile file){
+
+        if (!StringUtils.endsWithIgnoreCase(file.getOriginalFilename(), ".csv")) {
+            throw  new BusinessException(ErrorCode.INVALID_CSV_FORMAT);
+        }
+
+        try {
+            CSVReader reader = new CSVReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8));
+
+            // Use CsvToBean to map CSV records to a list of objects
+            CsvToBean<SafetyFacilityDto> csvToBean = new CsvToBeanBuilder<SafetyFacilityDto>(reader)
+                    .withType(SafetyFacilityDto.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            List<SafetyFacilityDto> parse = csvToBean.parse();
+            List<SafetyFacility> safetyBellList = parse.stream()
+                    .map(safeBellDto -> SafetyFacility.builder()
+                            .address(safeBellDto.getAddress())
+                            .location(new Point(safeBellDto.getLongitude(), safeBellDto.getLatitude()))
+                            .facilityType(FacilityType.SAFETY_BELL)
+                            .build())
+                    .collect(Collectors.toList());
+
+            safetyFacilityService.saveAll(safetyBellList);
+
+        } catch (IOException e) {
+            throw new BusinessException(ErrorCode.FILE_FORMAT);
+        }
+
+    }
+
+}

5a248683b7d6380c2816ad87ecabc5a4b571eb66
김윤욱 <reznov03048@gmail.com>
2023-09-13 17:22:35 +0900

    feat(SwaggerConfig) : #69 - Swagger 설정
    - Swagger dependency
    - SwaggerConfig 설정
    
    Related to: #69

---
+++ backend/enjoy-service/src/main/java/com/enjoyservice/global/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.enjoyservice.global.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .info(new Info().title("누비오-ENJOY")
+                        .description("누비오-ENJOY API")
+                        .version("1.0.0"));
+    }
+}

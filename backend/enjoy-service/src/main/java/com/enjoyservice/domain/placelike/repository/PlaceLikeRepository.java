c03cf19ca5903c7a5d90fb4fdeb869dbfe750770
김윤욱 <reznov03048@gmail.com>
2023-09-13 13:23:00 +0900

    feat(PlaceLikeRepository) : #69 - Place로 해당 Place의 PlaceLike 모두 조회하는 기능 구현
    Place로 해당 Place의 PlaceLike 모두 조회하는 기능 구현
    
    Related to: #69

--- backend/enjoy-service/src/main/java/com/enjoyservice/domain/placelike/repository/PlaceLikeRepository.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/placelike/repository/PlaceLikeRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface PlaceLikeRepository extends JpaRepository<PlaceLike, Long> {
 
     @Query("select count(pl) from PlaceLike pl where pl.place = :place")
     int getCountByPlace(@Param("place") Place place);
+
+    List<PlaceLike> findAllByPlace(Place place);
 }

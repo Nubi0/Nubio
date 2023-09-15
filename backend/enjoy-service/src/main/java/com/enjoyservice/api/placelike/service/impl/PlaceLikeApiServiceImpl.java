6ea8384458b37ce2eb972dcd794b26865e7ec230
김윤욱 <reznov03048@gmail.com>
2023-09-15 17:33:43 +0900

    feat(PlaceLikeApiService) : #82 - Place 좋아요 기능 구현
    Place 좋아요 기능 구현
    
    Related to: #82

---
+++ backend/enjoy-service/src/main/java/com/enjoyservice/api/placelike/service/impl/PlaceLikeApiServiceImpl.java
@@ -0,0 +1,35 @@
+package com.enjoyservice.api.placelike.service.impl;
+
+import com.enjoyservice.api.placelike.dto.PlaceLikeRes;
+import com.enjoyservice.api.placelike.service.PlaceLikeApiService;
+import com.enjoyservice.domain.place.entity.Place;
+import com.enjoyservice.domain.place.service.PlaceService;
+import com.enjoyservice.domain.placelike.entity.PlaceLike;
+import com.enjoyservice.domain.placelike.service.PlaceLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class PlaceLikeApiServiceImpl implements PlaceLikeApiService {
+
+    private final PlaceLikeService placeLikeService;
+    private final PlaceService placeService;
+
+    @Transactional
+    @Override
+    public PlaceLikeRes likePlace(String memberId, Long placeId) {
+        Place place = placeService.findById(placeId);
+        boolean result = placeLikeService.changePlaceLikeState(memberId, place);
+        List<PlaceLike> placeLikes = placeLikeService.findAllByPlace(place);
+        long likeCount = placeLikes.size();
+        return PlaceLikeRes.builder()
+                .likeCount(likeCount)
+                .likeFlag(result)
+                .build();
+    }
+}

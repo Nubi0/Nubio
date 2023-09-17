25c5bb951c2a031138497a0ed3aee29ad00acd32
김민규 <rrrr3321@gmail.com>
2023-09-18 01:14:31 +0900

    chore(database) : #93 - mongodb 연결
    - mongodb 연결 및 조회 확인
    
    Related to: #93

--- backend/recommendation-service/main.py
+++ backend/recommendation-service/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-
+from app import database
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/get-all")
+def get_all():
+    data = database.all()
+    return {"data" : data}

470b8256b0fa4698c97ab9da1db56034a36be0f6
김민규 <rrrr3321@gmail.com>
2023-09-18 23:20:03 +0900

    feat(main) : #93 - fastapi post 입력확인
    - json입력시 출력 확인
    
    Related to: #93

--- backend/recommendation-service/main.py
+++ backend/recommendation-service/main.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI
 from app import database
+from app import train
+from pydantic import BaseModel
+from typing import List
+
 app = FastAPI()
 
+
+class InputData(BaseModel):
+    words: List[str]
+    
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+# db불러와서 학습모델 저장
 @app.get("/get-all")
 def get_all():
     data = database.all()
+    train.recoCourse(data)
     return {"data" : data}
+
+# 입력한 정보와 비슷한 코스 응답
+@app.post("/get-list")
+async def process_words(request: InputData):
+    words = request.words
+    return {"result": words}

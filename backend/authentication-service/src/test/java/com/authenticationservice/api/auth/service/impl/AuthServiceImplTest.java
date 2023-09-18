62aa0fa6f8d1e154e7b9ae85040bbd3adafa39b3
이서현 <kathyleesh7@gmail.com>
2023-09-19 01:44:59 +0900

    test(AuthServiceImplTest) : #107 - 회원가입 실패 테스트 구현
    password와 passwordCheck가 일치하지 않아 실패하는지 테스트
    
    Related to: #107

--- backend/authentication-service/src/test/java/com/authenticationservice/api/auth/service/impl/AuthServiceImplTest.java
+++ backend/authentication-service/src/test/java/com/authenticationservice/api/auth/service/impl/AuthServiceImplTest.java
@@ -3,6 +3,7 @@ package com.authenticationservice.api.auth.service.impl;
 
 import com.authenticationservice.api.auth.dto.request.SignupReqDto;
 import com.authenticationservice.api.auth.dto.response.SignResDto;
+import com.authenticationservice.api.auth.exception.InvalidPasswordException;
 import com.authenticationservice.api.auth.service.AuthService;
 
 import org.junit.jupiter.api.DisplayName;
@@ -17,7 +18,6 @@ class AuthServiceImplTest {
 
     @Autowired
     private AuthService authService;
-
 
 
 
@@ -37,6 +37,25 @@ class AuthServiceImplTest {
         SignResDto result = authService.signup(request);
         // then
         assertNotNull(result);
+    }
+
+
+    @DisplayName("password와 passwordCheck가 일치하지 않아 실패한다.")
+    @Test
+    void passwordMismatch() {
+        // given
+        SignupReqDto request = SignupReqDto.builder()
+                .email("Member123@nubio.com")
+                .password("password")
+                .passwordCheck("passwordCheck")
+                .nickname("nickname")
+                .gender("male")
+                .birth("2000-01-01")
+                .build();
+        // when then
+        assertThrows(InvalidPasswordException.class, () -> {
+            authService.signup(request);
+        });
     }
 
 

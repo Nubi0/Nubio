a897a008bd565c6ed0deca95d75898ce9166cc45
김윤욱 <reznov03048@gmail.com>
2023-09-18 12:53:17 +0900

    feat(PlaceReview) : #94 - 장소 리뷰 관련 도메인 삭제
    장소 리뷰 관련 도메인 삭제
    
    Related to: #94

--- backend/enjoy-service/src/main/java/com/enjoyservice/domain/placereviewimage/repository/PlaceReviewImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.enjoyservice.domain.placereviewimage.repository;
-
-import com.enjoyservice.domain.placereviewimage.entity.PlaceReviewImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaceReviewImageRepository extends JpaRepository<PlaceReviewImage, Long> {
-}

80a517467724e63e267dad32b91e03340653b11d
이서현 <kathyleesh7@gmail.com>
2023-09-19 01:39:46 +0900

    test(AuthServiceImplTest) : #107 - 회원가입 테스트 구현
    자체 회원가입애 성공하는지 테스트
    
    Related to: #107

---
+++ backend/authentication-service/src/test/java/com/authenticationservice/api/auth/service/impl/AuthServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.authenticationservice.api.auth.service.impl;
+
+
+import com.authenticationservice.api.auth.dto.request.SignupReqDto;
+import com.authenticationservice.api.auth.dto.response.SignResDto;
+import com.authenticationservice.api.auth.service.AuthService;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AuthServiceImplTest {
+
+    @Autowired
+    private AuthService authService;
+
+
+
+
+    @DisplayName("Nubio 자체 회원가입에 성공한다.")
+    @Test
+    void signupSuccessful() {
+        // given
+        SignupReqDto request = SignupReqDto.builder()
+                .email("Member123@nubio.com")
+                .password("password")
+                .passwordCheck("password")
+                .nickname("nickname")
+                .gender("male")
+                .birth("2000-01-01")
+                .build();
+        // when
+        SignResDto result = authService.signup(request);
+        // then
+        assertNotNull(result);
+    }
+
+
+
+}

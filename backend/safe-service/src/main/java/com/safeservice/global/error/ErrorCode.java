9977ffc719f0eb6b6225affeb6d87c8d965f62b3
jyj143 <jyj1143@gmail.com>
2023-09-18 14:45:09 +0900

    feat(ErrorCode) : #90 - 파일 형식이 잘못된 경우 에러 코드 생서
    - 파일 형식이 잘못된 경우 에러 코드 생서
    
    Related to: #90

--- backend/safe-service/src/main/java/com/safeservice/global/error/ErrorCode.java
+++ backend/safe-service/src/main/java/com/safeservice/global/error/ErrorCode.java
@@ -8,12 +8,11 @@ public enum ErrorCode {
 
     TEST_EXCEPTION(HttpStatus.UNAUTHORIZED, "TEST-000", "에러 테스트"),
 
+    // 노드
+    FACILITY__NOT_EXIST(HttpStatus.BAD_REQUEST, "N-001", "거리 이내에 안전시설물이 없습니다."),
+
     //안전시설
     FACILITY_TYPE_NOT_EXIST(HttpStatus.BAD_REQUEST, "F-001", "해당 시설물의 Type은 없습니다."),
-
-    // 파일
-    FILE_FORMAT(HttpStatus.BAD_REQUEST, "F-001", "파일 형식이 잘못되었습니다."),
-    INVALID_CSV_FORMAT(HttpStatus.BAD_REQUEST, "F-002", "csv 파일 형식이 잘못되었습니다."),
 
     // 안전벨
     INVALID_ADDRESS(HttpStatus.BAD_REQUEST, "SB-001", "주소 길이가 잘못되었습니다."),
@@ -34,6 +33,8 @@ public enum ErrorCode {
     EMPTY_FILE(HttpStatus.BAD_REQUEST, "F-001", "빈 파일은 제출할 수 없습니다."),
     FILE_AMOUNTS_LIMIT(HttpStatus.BAD_REQUEST, "F-002", "제출할 수 있는 파일 수를 초과했습니다."),
     FILE_SIZE_LIMIT(HttpStatus.BAD_REQUEST, "F-003", "제출할 수 있는 파일 사이즈를 초과했습니다."),
+    FILE_FORMAT(HttpStatus.BAD_REQUEST, "F-004", "파일 형식이 잘못되었습니다."),
+    INVALID_CSV_FORMAT(HttpStatus.BAD_REQUEST, "F-005", "csv 파일 형식이 잘못되었습니다."),
     ;
 
     ErrorCode(HttpStatus httpStatus, String errorCode, String message) {

d45aebc0562ecbb819f85da5601e60ed71d2e7ad
김윤욱 <reznov03048@gmail.com>
2023-09-18 12:39:14 +0900

    feat(PlaceReviewServiceImpl) : #94 - 트랜잭션 추가
    트랜잭션 추가
    
    Related to: #94

--- backend/enjoy-service/src/main/java/com/enjoyservice/domain/placereview/service/impl/PlaceReviewServiceImpl.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/placereview/service/impl/PlaceReviewServiceImpl.java
@@ -6,14 +6,17 @@ import com.enjoyservice.domain.placereview.service.PlaceReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @Slf4j
 @RequiredArgsConstructor
 public class PlaceReviewServiceImpl implements PlaceReviewService {
 
     private final PlaceReviewRepository placeReviewRepository;
 
+    @Transactional
     @Override
     public void save(PlaceReview placeReview) {
         placeReviewRepository.save(placeReview);
--- backend/enjoy-service/src/main/java/com/enjoyservice/domain/placereviewimage/service/impl/PlaceReviewImageServiceImpl.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/placereviewimage/service/impl/PlaceReviewImageServiceImpl.java
@@ -6,14 +6,17 @@ import com.enjoyservice.domain.placereviewimage.service.PlaceReviewImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @Slf4j
 @RequiredArgsConstructor
 public class PlaceReviewImageServiceImpl implements PlaceReviewImageService {
 
     private final PlaceReviewImageRepository placeReviewImageRepository;
 
+    @Transactional
     @Override
     public void save(PlaceReviewImage placeReviewImage) {
         placeReviewImageRepository.save(placeReviewImage);

f1067e1199b756ccefe09930735ad003c5abe0b2
김윤욱 <reznov03048@gmail.com>
2023-09-19 09:56:45 +0900

    feat(CourseApiServiceImpl) : #97 - Course 생성 기능 구현
    Course 생성 기능 구현
    
    Related to: #97

--- backend/enjoy-service/src/main/java/com/enjoyservice/api/course/service/impl/CourseApiServiceImpl.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/api/course/service/impl/CourseApiServiceImpl.java
@@ -3,26 +3,23 @@ package com.enjoyservice.api.course.service.impl;
 import com.enjoyservice.api.course.dto.CourseCreateReq;
 import com.enjoyservice.api.course.service.CourseApiService;
 import com.enjoyservice.domain.course.entity.Course;
-import com.enjoyservice.domain.course.entity.constant.Region;
-import com.enjoyservice.domain.course.entity.type.Content;
-import com.enjoyservice.domain.course.entity.type.PublicFlag;
-import com.enjoyservice.domain.course.entity.type.Title;
 import com.enjoyservice.domain.course.service.CourseService;
 import com.enjoyservice.domain.courseplacesequence.entity.CoursePlaceSequence;
-import com.enjoyservice.domain.courseplacesequence.entity.type.SequenceNumber;
 import com.enjoyservice.domain.courseplacesequence.service.CoursePlaceSequenceService;
+import com.enjoyservice.domain.coursetag.entity.CourseTag;
+import com.enjoyservice.domain.coursetag.service.CourseTagService;
 import com.enjoyservice.domain.place.entity.Place;
 import com.enjoyservice.domain.place.service.PlaceService;
+import com.enjoyservice.domain.tag.entity.Tag;
+import com.enjoyservice.domain.tag.entity.type.Name;
+import com.enjoyservice.domain.tag.service.TagService;
 import com.enjoyservice.mapper.course.CourseMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -34,10 +31,12 @@ public class CourseApiServiceImpl implements CourseApiService {
     private final CourseService courseService;
     private final PlaceService placeService;
     private final CoursePlaceSequenceService coursePlaceSequenceService;
+    private final TagService tagService;
+    private final CourseTagService courseTagService;
 
 //    @Transactional
     @Override
-    public void createCourse(CourseCreateReq request, String memberId) {
+    public void createCourse(final CourseCreateReq request, final String memberId) {
         // 코스 저장
         Course course = CourseMapper.courseCreateReqToCourse(request, memberId);
         Course savedCourse = courseService.save(course);
@@ -54,7 +53,32 @@ public class CourseApiServiceImpl implements CourseApiService {
         log.info("CoursePlaceSequence 목록 저장(CourseApiServiceImpl)");
 
         // 코스 태그 저장
+        // 태그 잇는지 확인
+        linkCourseTag(request, course);
+        log.info("Course에 Tag 연결(CourseApiServiceImpl)");
+    }
 
+    private void linkCourseTag(CourseCreateReq request, Course course) {
+        for(CourseCreateReq.TagInfo tagInfo : request.getCourseTags()) {
+            Optional<Tag> opTag = tagService.findByName(Name.from(tagInfo.getValue()));
+            // 있는건 바로 연결
+            if(opTag.isPresent()) {
+                linkExistedTag(course, opTag.get());
+            } else { // 없는건 새로 저장
+                linkNewTag(course, tagInfo.getValue());
+            }
+        }
+    }
+
+    private void linkNewTag(Course course, String tagName) {
+        Tag tag = Tag.from(tagName);
+        Tag savedTag = tagService.save(tag);
+        linkExistedTag(course, savedTag);
+    }
+
+    private void linkExistedTag(Course course, Tag tag) {
+        CourseTag courseTag = CourseTag.from(course, tag);
+        courseTagService.save(courseTag);
     }
 
     private Map<Long, Integer> mappingSequence(List<CourseCreateReq.PlaceInfo> placeInfos) {

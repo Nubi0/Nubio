b89bae74d26363e22d47087e0712afee4d43bf92
김윤욱 <reznov03048@gmail.com>
2023-09-19 11:02:22 +0900

    feat(CourseRepository) : #112 - Region 으로 Course 목록 조회 기능
    Region 으로 Course 목록 조회 기능
    
    Related to: #112

--- backend/enjoy-service/src/main/java/com/enjoyservice/domain/course/repository/CourseRepository.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/course/repository/CourseRepository.java
@@ -1,7 +1,12 @@
 package com.enjoyservice.domain.course.repository;
 
 import com.enjoyservice.domain.course.entity.Course;
+import com.enjoyservice.domain.course.entity.constant.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findAllByRegion(Region region);
 }

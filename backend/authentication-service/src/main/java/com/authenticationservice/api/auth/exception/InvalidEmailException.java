2f6039858963bca284956dd56ba08a306dd5675d
이서현 <kathyleesh7@gmail.com>
2023-09-19 09:58:04 +0900

    fix(Exception) : #46 - InvalidException 추가
    InvalidException으로 묶어 한단계 더 상속받게 변경
    
    Related to: #46

--- backend/authentication-service/src/main/java/com/authenticationservice/api/auth/exception/InvalidEmailException.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/api/auth/exception/InvalidEmailException.java
@@ -2,8 +2,10 @@ package com.authenticationservice.api.auth.exception;
 
 import com.authenticationservice.global.error.ErrorCode;
 import com.authenticationservice.global.error.exception.BusinessException;
+import com.authenticationservice.global.error.exception.InvalidException;
+import com.authenticationservice.global.error.exception.ValidationException;
 
-public class InvalidEmailException extends BusinessException {
+public class InvalidEmailException extends InvalidException {
 
     public InvalidEmailException(ErrorCode errorCode) {
         super(errorCode);
--- backend/authentication-service/src/main/java/com/authenticationservice/api/auth/exception/InvalidPasswordException.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/api/auth/exception/InvalidPasswordException.java
@@ -1,9 +1,12 @@
 package com.authenticationservice.api.auth.exception;
 
+import com.authenticationservice.domain.member.exception.InvalidBirthFormatException;
 import com.authenticationservice.global.error.ErrorCode;
 import com.authenticationservice.global.error.exception.BusinessException;
+import com.authenticationservice.global.error.exception.InvalidException;
+import com.authenticationservice.global.error.exception.ValidationException;
 
-public class InvalidPasswordException extends BusinessException {
+public class InvalidPasswordException extends InvalidException {
 
     public InvalidPasswordException(ErrorCode errorCode) {
         super(errorCode);

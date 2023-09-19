21e2a43fdbb8dbf23a8e45dab91bef6de9d98133
이서현 <kathyleesh7@gmail.com>
2023-09-19 17:47:00 +0900

    fix(SecurityConfig) : #110 - cors 해제
    cors kong에 부여
    
    Related to: #110

--- backend/authentication-service/src/main/java/com/authenticationservice/global/config/SecurityConfig.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/global/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors
-                        .configurationSource(webConfig.corsConfigurationSource()))
+                        .disable())
                 .csrf(csrf -> csrf
                                 .disable()
                 )

b0f4074e9fc46c1535383e44c1121eb6103d5442
김윤욱 <reznov03048@gmail.com>
2023-09-19 11:07:32 +0900

    feat(CourseService) : #112 - Region 으로 Course 목록 조회 기능
    Region 으로 Course 목록 조회 기능
    
    Related to: #112

--- backend/enjoy-service/src/main/java/com/enjoyservice/domain/course/service/CourseService.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/course/service/CourseService.java
@@ -1,8 +1,13 @@
 package com.enjoyservice.domain.course.service;
 
 import com.enjoyservice.domain.course.entity.Course;
+import com.enjoyservice.domain.course.entity.constant.Region;
+
+import java.util.List;
 
 public interface CourseService {
 
     Course save(Course course);
+
+    List<Course> findAllByRegion(Region region);
 }
--- backend/enjoy-service/src/main/java/com/enjoyservice/domain/course/service/impl/CourseServiceImpl.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/course/service/impl/CourseServiceImpl.java
@@ -1,12 +1,15 @@
 package com.enjoyservice.domain.course.service.impl;
 
 import com.enjoyservice.domain.course.entity.Course;
+import com.enjoyservice.domain.course.entity.constant.Region;
 import com.enjoyservice.domain.course.repository.CourseRepository;
 import com.enjoyservice.domain.course.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -22,5 +25,10 @@ public class CourseServiceImpl implements CourseService {
         Course savedCourse = courseRepository.save(course);
         log.info("코스 저장 완료(CourseServiceImpl) : courseId = {}, memberId = {}", savedCourse.getId(), savedCourse.getMemberId());
         return savedCourse;
+    }
+
+    @Override
+    public List<Course> findAllByRegion(Region region) {
+        return courseRepository.findAllByRegion(region);
     }
 }

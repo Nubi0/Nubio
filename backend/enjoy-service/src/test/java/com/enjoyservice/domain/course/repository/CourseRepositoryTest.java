655fa5a7e4b2069f8eb682f25ebc8578f579949c
김윤욱 <reznov03048@gmail.com>
2023-09-19 13:52:02 +0900

    test(CourseServiceImplTest) : #112 - Course 조회할 때 fetch로 Place 조회 기능 test
    Course 조회할 때 fetch로 Place 조회 기능 test
    
    Related to: #112

--- backend/enjoy-service/src/test/java/com/enjoyservice/domain/course/repository/CourseRepositoryTest.java
+++ backend/enjoy-service/src/test/java/com/enjoyservice/domain/course/repository/CourseRepositoryTest.java
@@ -5,7 +5,16 @@ import com.enjoyservice.domain.course.entity.constant.Region;
 import com.enjoyservice.domain.course.entity.type.Content;
 import com.enjoyservice.domain.course.entity.type.PublicFlag;
 import com.enjoyservice.domain.course.entity.type.Title;
+import com.enjoyservice.domain.courseplacesequence.entity.CoursePlaceSequence;
+import com.enjoyservice.domain.courseplacesequence.entity.type.SequenceNumber;
+import com.enjoyservice.domain.courseplacesequence.repository.CoursePlaceSequenceRepository;
+import com.enjoyservice.domain.place.entity.Place;
+import com.enjoyservice.domain.place.entity.constant.GroupCode;
+import com.enjoyservice.domain.place.entity.constant.GroupName;
+import com.enjoyservice.domain.place.entity.type.*;
+import com.enjoyservice.domain.place.repository.PlaceRepository;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +22,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
-import org.springframework.data.domain.Pageable;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -26,9 +33,66 @@ class CourseRepositoryTest {
 
     @Autowired
     private CourseRepository courseRepository;
-
+    @Autowired
+    private PlaceRepository placeRepository;
+    @Autowired
+    private CoursePlaceSequenceRepository coursePlaceSequenceRepository;
     @Autowired
     private EntityManager em;
+
+    private List<Course> savedBeforeCourses;
+    private List<Place> savedBeforePlaces;
+    private List<CoursePlaceSequence> savedBeforeCoursePlaceSequences;
+
+    @BeforeEach
+    void setUp() {
+        // 코스 저장
+        int courseSize = 2;
+        List<Course> courses = new ArrayList<>();
+        for(int i = 1; i <= courseSize; i++) {
+            Course course = generateCourse(i, Region.DAEGU);
+            courses.add(course);
+        }
+        for(int i = courseSize + 1; i <= courseSize + 3; i++) {
+            Course course = generateCourse(i, Region.BUSAN);
+            courses.add(course);
+        }
+        savedBeforeCourses = courseRepository.saveAllAndFlush(courses);
+        em.clear();
+
+        // 장소 저장
+        int placeSize = 5;
+        List<Place> places = new ArrayList<>();
+        for(int i = 1; i <= placeSize; i++) {
+            Place place = generatePlace(i, GroupCode.CD7, GroupName.카페);
+            places.add(place);
+        }
+        savedBeforePlaces = placeRepository.saveAllAndFlush(places);
+        em.clear();
+
+        // CoursePlaceSequence 저장
+        // 1번 코스에 장소 3개 추가
+        List<CoursePlaceSequence> coursePlaceSequences = new ArrayList<>();
+        for(int seq = 1; seq <= 3; seq++) {
+            CoursePlaceSequence coursePlaceSequence = CoursePlaceSequence.builder()
+                    .place(savedBeforePlaces.get(seq - 1))
+                    .course(savedBeforeCourses.get(0))
+                    .sequenceNumber(SequenceNumber.from(seq))
+                    .build();
+            coursePlaceSequences.add(coursePlaceSequence);
+        }
+        // 2번 코스에 장소 2개 추가
+        for(int seq = 1; seq <= 2; seq++) {
+            CoursePlaceSequence coursePlaceSequence = CoursePlaceSequence.builder()
+                    .place(savedBeforePlaces.get(seq + 2))
+                    .course(savedBeforeCourses.get(1))
+                    .sequenceNumber(SequenceNumber.from(seq))
+                    .build();
+            coursePlaceSequences.add(coursePlaceSequence);
+        }
+        savedBeforeCoursePlaceSequences = coursePlaceSequenceRepository.saveAllAndFlush(coursePlaceSequences);
+        em.clear();
+    }
 
     private Course generateCourse(int index, Region region) {
         return Course.builder()
@@ -40,31 +104,40 @@ class CourseRepositoryTest {
                 .build();
     }
 
-    @DisplayName("Region으로 Course 목록 조회")
+    private Place generatePlace(int index, GroupCode groupCode, GroupName groupName) {
+        return Place.builder()
+                .kakaoId(KakaoId.from(index))
+                .name(Name.from("place" + index))
+                .category(Category.from(groupCode, groupName, Detail.from("category detail" + index)))
+                .phone(Phone.from("010-1111-111" + index))
+                .url(Url.from("abc" + index + ".nubio.com"))
+                .address(Address.builder()
+                        .name("주소" + index)
+                        .roadName(RoadName.from("도로명주소" + index)).build()
+                )
+                .position(Position.builder()
+                        .latitude(Latitude.from(Double.valueOf("37.263906325599" + index)))
+                        .longitude(Longitude.from(Double.valueOf("127.032310032317" + index)))
+                        .build())
+                .active(Active.from(true))
+                .build();
+    }
+
+    @DisplayName("Region으로 Course 목록 조회 : CoursePlaceSequence, Place fetch join")
     @Test
     void findAllByRegion() {
         // given
-        int courseSize = 3;
-        List<Course> courses = new ArrayList<>();
-        for(int i = 1; i <= courseSize; i++) {
-            Course course = generateCourse(i, Region.DAEGU);
-            courses.add(course);
-        }
-        for(int i = courseSize + 1; i <= courseSize + 3; i++) {
-            Course course = generateCourse(i, Region.BUSAN);
-            courses.add(course);
-        }
-        List<Course> savedCourses = courseRepository.saveAllAndFlush(courses);
-        em.clear();
         // when
-        List<Course> result = courseRepository.findAllByRegion(Region.from("DAEGU"), PageRequest.of(0, 5));
+        List<Course> result = courseRepository.findAllByRegionFetchPlace(Region.from("DAEGU"), PageRequest.of(0, 5));
         // then
-        assertThat(result).hasSize(courseSize)
+        assertThat(result).hasSize(5)
                 .extracting("title.value", "content.value", "region")
                 .containsExactlyInAnyOrder(
                         tuple("courseTitle1", "courseContent1", Region.DAEGU),
+                        tuple("courseTitle1", "courseContent1", Region.DAEGU),
+                        tuple("courseTitle1", "courseContent1", Region.DAEGU),
                         tuple("courseTitle2", "courseContent2", Region.DAEGU),
-                        tuple("courseTitle3", "courseContent3", Region.DAEGU)
+                        tuple("courseTitle2", "courseContent2", Region.DAEGU)
                 );
     }
 }
--- backend/enjoy-service/src/test/java/com/enjoyservice/domain/course/service/impl/CourseServiceImplTest.java
+++ backend/enjoy-service/src/test/java/com/enjoyservice/domain/course/service/impl/CourseServiceImplTest.java
@@ -7,7 +7,16 @@ import com.enjoyservice.domain.course.entity.type.PublicFlag;
 import com.enjoyservice.domain.course.entity.type.Title;
 import com.enjoyservice.domain.course.repository.CourseRepository;
 import com.enjoyservice.domain.course.service.CourseService;
+import com.enjoyservice.domain.courseplacesequence.entity.CoursePlaceSequence;
+import com.enjoyservice.domain.courseplacesequence.entity.type.SequenceNumber;
+import com.enjoyservice.domain.courseplacesequence.repository.CoursePlaceSequenceRepository;
+import com.enjoyservice.domain.place.entity.Place;
+import com.enjoyservice.domain.place.entity.constant.GroupCode;
+import com.enjoyservice.domain.place.entity.constant.GroupName;
+import com.enjoyservice.domain.place.entity.type.*;
+import com.enjoyservice.domain.place.repository.PlaceRepository;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,12 +38,69 @@ class CourseServiceImplTest {
 
     @Autowired
     private CourseService courseService;
-
     @Autowired
     private CourseRepository courseRepository;
+    @Autowired
+    private PlaceRepository placeRepository;
+    @Autowired
+    private CoursePlaceSequenceRepository coursePlaceSequenceRepository;
 
     @Autowired
     private EntityManager em;
+
+    private List<Course> savedBeforeCourses;
+    private List<Place> savedBeforePlaces;
+    private List<CoursePlaceSequence> savedBeforeCoursePlaceSequences;
+
+    @BeforeEach
+    void setUp() {
+        // 코스 저장
+        int courseSize = 2;
+        List<Course> courses = new ArrayList<>();
+        for(int i = 1; i <= courseSize; i++) {
+            Course course = generateCourse(i, Region.DAEGU);
+            courses.add(course);
+        }
+        for(int i = courseSize + 1; i <= courseSize + 3; i++) {
+            Course course = generateCourse(i, Region.BUSAN);
+            courses.add(course);
+        }
+        savedBeforeCourses = courseRepository.saveAllAndFlush(courses);
+        em.clear();
+
+        // 장소 저장
+        int placeSize = 5;
+        List<Place> places = new ArrayList<>();
+        for(int i = 1; i <= placeSize; i++) {
+            Place place = generatePlace(i, GroupCode.CD7, GroupName.카페);
+            places.add(place);
+        }
+        savedBeforePlaces = placeRepository.saveAllAndFlush(places);
+        em.clear();
+
+        // CoursePlaceSequence 저장
+        // 1번 코스에 장소 3개 추가
+        List<CoursePlaceSequence> coursePlaceSequences = new ArrayList<>();
+        for(int seq = 1; seq <= 3; seq++) {
+            CoursePlaceSequence coursePlaceSequence = CoursePlaceSequence.builder()
+                    .place(savedBeforePlaces.get(seq - 1))
+                    .course(savedBeforeCourses.get(0))
+                    .sequenceNumber(SequenceNumber.from(seq))
+                    .build();
+            coursePlaceSequences.add(coursePlaceSequence);
+        }
+        // 2번 코스에 장소 2개 추가
+        for(int seq = 1; seq <= 2; seq++) {
+            CoursePlaceSequence coursePlaceSequence = CoursePlaceSequence.builder()
+                    .place(savedBeforePlaces.get(seq + 2))
+                    .course(savedBeforeCourses.get(1))
+                    .sequenceNumber(SequenceNumber.from(seq))
+                    .build();
+            coursePlaceSequences.add(coursePlaceSequence);
+        }
+        savedBeforeCoursePlaceSequences = coursePlaceSequenceRepository.saveAllAndFlush(coursePlaceSequences);
+        em.clear();
+    }
 
     private Course generateCourse(int index, Region region) {
         return Course.builder()
@@ -46,31 +112,40 @@ class CourseServiceImplTest {
                 .build();
     }
 
-    @DisplayName("Region으로 Course 목록 조회")
+    private Place generatePlace(int index, GroupCode groupCode, GroupName groupName) {
+        return Place.builder()
+                .kakaoId(KakaoId.from(index))
+                .name(Name.from("place" + index))
+                .category(Category.from(groupCode, groupName, Detail.from("category detail" + index)))
+                .phone(Phone.from("010-1111-111" + index))
+                .url(Url.from("abc" + index + ".nubio.com"))
+                .address(Address.builder()
+                        .name("주소" + index)
+                        .roadName(RoadName.from("도로명주소" + index)).build()
+                )
+                .position(Position.builder()
+                        .latitude(Latitude.from(Double.valueOf("37.263906325599" + index)))
+                        .longitude(Longitude.from(Double.valueOf("127.032310032317" + index)))
+                        .build())
+                .active(Active.from(true))
+                .build();
+    }
+
+    @DisplayName("Region으로 Course 목록 조회 : CoursePlaceSequence, Place fetch join")
     @Test
     void findAllByRegion() {
         // given
-        int courseSize = 3;
-        List<Course> courses = new ArrayList<>();
-        for(int i = 1; i <= courseSize; i++) {
-            Course course = generateCourse(i, Region.DAEGU);
-            courses.add(course);
-        }
-        for(int i = courseSize + 1; i <= courseSize + 3; i++) {
-            Course course = generateCourse(i, Region.BUSAN);
-            courses.add(course);
-        }
-        List<Course> savedCourses = courseRepository.saveAllAndFlush(courses);
-        em.clear();
         // when
-        List<Course> result = courseService.findAllByRegion(Region.from("DAEGU"), PageRequest.of(0, 3));
+        List<Course> result = courseService.findAllByRegionFetchPlace(Region.from("DAEGU"), PageRequest.of(0, 5));
         // then
-        assertThat(result).hasSize(courseSize)
+        assertThat(result).hasSize(5)
                 .extracting("title.value", "content.value", "region")
                 .containsExactlyInAnyOrder(
                         tuple("courseTitle1", "courseContent1", Region.DAEGU),
+                        tuple("courseTitle1", "courseContent1", Region.DAEGU),
+                        tuple("courseTitle1", "courseContent1", Region.DAEGU),
                         tuple("courseTitle2", "courseContent2", Region.DAEGU),
-                        tuple("courseTitle3", "courseContent3", Region.DAEGU)
+                        tuple("courseTitle2", "courseContent2", Region.DAEGU)
                 );
     }
 }

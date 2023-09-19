4992baeafc996201bd954eeb659536daf654b88d
김윤욱 <reznov03048@gmail.com>
2023-09-19 12:28:00 +0900

    feat(CourseRepository) : #112 - Pageable 추가
    Pageable 추가
    
    Related to: #112

--- backend/enjoy-service/src/main/java/com/enjoyservice/domain/course/repository/CourseRepository.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/course/repository/CourseRepository.java
@@ -4,9 +4,10 @@ import com.enjoyservice.domain.course.entity.Course;
 import com.enjoyservice.domain.course.entity.constant.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
-    List<Course> findAllByRegion(Region region);
+    List<Course> findAllByRegion(Region region, Pageable pageable);
 }

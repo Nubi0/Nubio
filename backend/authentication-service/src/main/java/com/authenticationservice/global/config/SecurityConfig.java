cfea4464c137d36ae5b4a65701b3aa6eae8c1ff9
이서현 <kathyleesh7@gmail.com>
2023-09-19 15:00:36 +0900

    feat(WebConfig) : #110 - cors origin 추가
    'http://nubi0.com', 'https://nubi0.com', 'http://ec2-43-201-30-17.ap-northeast-2.compute.amazonaws.com/8000'
    
    ãRelated to: #110

--- backend/authentication-service/src/main/java/com/authenticationservice/global/config/SecurityConfig.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/global/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors
-                        .disable())
+                        .configurationSource(webConfig.corsConfigurationSource()))
                 .csrf(csrf -> csrf
                                 .disable()
                 )
--- backend/authentication-service/src/main/java/com/authenticationservice/global/config/WebConfig.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/global/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://nubi0.com", "https://nubi0.com", "http://ec2-43-201-30-17.ap-northeast-2.compute.amazonaws.com/8000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));
         configuration.setAllowCredentials(true); // 허용된 도메인에 쿠키를 전송하도록 허용

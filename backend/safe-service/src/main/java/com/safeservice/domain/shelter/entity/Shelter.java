02c034addd1c9648a9b84e9245fd8f4cc0b16038
jyj143 <jyj1143@gmail.com>
2023-09-19 15:12:41 +0900

    feat(Shelter) : #114 - Creat Shelter Entity
    - Creat Shelter Entity
    
    Related to: #114

---
+++ backend/safe-service/src/main/java/com/safeservice/domain/shelter/entity/Shelter.java
@@ -0,0 +1,47 @@
+package com.safeservice.domain.shelter.entity;
+
+import com.safeservice.domain.shelter.constant.ShelterType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.geo.Point;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Getter
+@NoArgsConstructor
+@Document("shelter")
+public class Shelter {
+
+    @MongoId
+    private String id;
+
+    @Field(name = "address")
+    private String address;
+
+    @Field(name = "name")
+    private String name;
+
+    @Field(name = "phone_num")
+    private String phoneNum;
+
+    @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
+    private Point location;
+
+    @Field(name = "shelter-type")
+    private ShelterType shelterType;
+
+    @Builder
+    public Shelter(String id, String address, String name, String phoneNum, Point location, ShelterType shelterType) {
+        this.id = id;
+        this.address = address;
+        this.name = name;
+        this.phoneNum = phoneNum;
+        this.location = location;
+        this.shelterType = shelterType;
+    }
+
+}

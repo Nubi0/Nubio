6c7f601e61a7e18afff26cb949f0bfb30c22c121
김윤욱 <reznov03048@gmail.com>
2023-09-19 16:07:14 +0900

    feat(CourseLikeRepository) : #112 - CourseLikeRepository 생성
    CourseLikeRepository 생성
    
    Related to: #112

---
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/courselike/repository/CourseLikeRepository.java
@@ -0,0 +1,7 @@
+package com.enjoyservice.domain.courselike.repository;
+
+import com.enjoyservice.domain.courselike.entity.CourseLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseLikeRepository extends JpaRepository<CourseLike, Long> {
+}

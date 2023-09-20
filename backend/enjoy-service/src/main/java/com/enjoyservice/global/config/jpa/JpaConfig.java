347b5330dfabbee303e574367f3022aa8a4dcd83
jyj143 <jyj1143@gmail.com>
2023-09-20 14:51:23 +0900

    feat(MongoDBConfig) : #126 - mongoDB설정
    - mongoDB설정
    - spring data jpa
    - spring data mongodb
    
    Closes: #126

--- backend/enjoy-service/src/main/java/com/enjoyservice/global/config/jpa/JpaConfig.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/global/config/jpa/JpaConfig.java
@@ -1,14 +1,28 @@
 package com.enjoyservice.global.config.jpa;
 
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaAuditing
+@EnableTransactionManagement
+@RequiredArgsConstructor
 public class JpaConfig {
 
+    private final EntityManagerFactory entityManagerFactory;
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
     @Bean
     public AuditorAware<String> auditorAware() {
         return new AuditorAwareImpl();

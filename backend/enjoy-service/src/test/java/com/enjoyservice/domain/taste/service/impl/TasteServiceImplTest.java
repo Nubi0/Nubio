3f1354d4b652af96cbcea087e4dbfff4fd85d110
김민규 <melon212@naver.com>
2023-09-20 13:14:35 +0900

    test(TasteTest) : #123 - TasteTest
    - Type, DetailType 테스트
    - TasteServiceImpl 테스트
    
    Related to: #123

---
+++ backend/enjoy-service/src/test/java/com/enjoyservice/domain/taste/service/impl/TasteServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.enjoyservice.domain.taste.service.impl;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class TasteServiceImplTest {
+
+    @DisplayName("취향 저장에 성공한다.")
+    @Test
+    void saveTaste() {
+    }
+}

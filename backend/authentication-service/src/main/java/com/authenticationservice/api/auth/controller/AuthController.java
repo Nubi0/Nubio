5cc8253ace9a3b9809a04d17d982975b2bb34aef
이서현 <kathyleesh7@gmail.com>
2023-09-21 17:51:36 +0900

    docs(AuthController) : #110 - swagger 설정
    swagger error code 설정
    
    Related to: #110

--- backend/authentication-service/src/main/java/com/authenticationservice/api/auth/controller/AuthController.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/api/auth/controller/AuthController.java
@@ -32,18 +32,32 @@ public class AuthController {
     final AuthService authService;
 
     @Operation(summary = "회원가입", description = "start/v1/member/signup\n\n" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description =  "CREATED"),
+            @ApiResponse(responseCode = "M-008", description =  "INVALID_PASSWORD_CHECK", content = @Content(examples = @ExampleObject(value = "{\"errorCode\": \"M-008\", \"errorMessage\": \"비밀번호가 일치하지 않습니다.\"}"))),
+            @ApiResponse(responseCode = "M-009", description =  "EMAIL_IS_EXISTS", content = @Content(examples = @ExampleObject(value = "{\"errorCode\": \"M-009\", \"errorMessage\": \"존재하는 이메일입니다.\"}")))
+    })
     @PostMapping("/signup")
     public ApiResponseEntity<SignResDto> signup(@Valid @RequestBody SignupReqDto signupReqDto) {
         return ApiResponseEntity.created(authService.signup(signupReqDto));
     }
 
     @Operation(summary = "로그인", description = "start/v1/member/login\n\n" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description =  "CREATED"),
+            @ApiResponse(responseCode = "M-007", description =  "MEMBER_NOT_EXISTS", content = @Content(examples = @ExampleObject(value = "{\"errorCode\": \"M-007\", \"errorMessage\": \"해당 회원은 존재하지 않습니다.\"}"))),
+            @ApiResponse(responseCode = "M-008", description =  "INVALID_PASSWORD_CHECK", content = @Content(examples = @ExampleObject(value = "{\"errorCode\": \"M-008\", \"errorMessage\": \"비밀번호가 일치하지 않습니다.\"}"))),
+    })
     @PostMapping("/login")
     public ApiResponseEntity<SignResDto> login(@Valid @RequestBody LoginReqDto loginReqDto) {
         return ApiResponseEntity.ok(authService.login(loginReqDto));
     }
 
     @Operation(summary = "로그아웃", description = "start/v1/member/logout\n\n" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description =  "CREATED"),
+            @ApiResponse(responseCode = "M-007", description =  "MEMBER_NOT_EXISTS", content = @Content(examples = @ExampleObject(value = "{\"errorCode\": \"M-007\", \"errorMessage\": \"해당 회원은 존재하지 않습니다.\"}")))
+    })
     @PostMapping("/logout")
     public ApiResponseEntity<String> logout(@MemberInfo MemberInfoDto memberInfo, HttpServletRequest httpServletRequest) {
         String authorizationHeader = httpServletRequest.getHeader("Authorization");

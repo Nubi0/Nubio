53b44284f95839bfde907c8bade8f76466965018
윤태웅 <omygog@naver.com>
2023-09-22 01:03:43 +0900

    feat(SignUpForm) : #131 - Swal 추가
    회원가입, 로그인 Swal  추가
    
    Related to: #131

--- Frontend/nubio/src/components/user/signUp/SignUpForm.tsx
+++ Frontend/nubio/src/components/user/signUp/SignUpForm.tsx
@@ -4,12 +4,13 @@ import {
   WomanIcon,
   GenderWrapper,
   SubmitButton,
-} from "../../../styles/SSignUpPage";
-import { useRef, MouseEvent, useState } from "react";
-import useInput from "../../../hooks/useInput";
-import { useSelector } from "react-redux";
+} from '../../../styles/SSignUpPage';
+import { useRef, MouseEvent, useState } from 'react';
+import useInput from '../../../hooks/useInput';
+import { useSelector } from 'react-redux';
 import axios from 'axios';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const SignUpForm = () => {
   const [email, onChangeEmail] = useInput('');
@@ -28,62 +29,91 @@ const SignUpForm = () => {
     nickname: nickName,
     birth,
     gender,
-  }
+  };
 
   const signUp = async (e: any) => {
     e.preventDefault();
-    await axios.post('https://nubi0.com/start/v1/member/signup', data)
-                .then((res) => {
-                  // Todo : 회원가입 성공 Swal
-                  navigate('/login');
-                })
-                .catch((err) => {
-                  console.log(err);
-                })
-  }
+    await axios
+      .post('https://nubi0.com/start/v1/member/signup', data)
+      .then((res) => {
+        // Todo : 회원가입 성공 Swal
+        Swal.fire({
+          title: '회원가입 성공',
+          text: 'Nubio',
+          icon: 'success',
+        }).then((res) => {
+          if (res.isConfirmed) {
+            navigate('/login');
+          }
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   // 남자 아이콘
-  const manUrl = process.env.PUBLIC_URL + "/assets/man.png";
+  const manUrl = process.env.PUBLIC_URL + '/assets/man.png';
   const [manCheck, setManCheck] = useState<boolean>(true);
   const manInputRef = useRef<HTMLInputElement | null>(null);
-  const manId = manCheck ? "manCheck" : "manUncheck";
+  const manId = manCheck ? 'manCheck' : 'manUncheck';
   const handleManIconClick = (event: MouseEvent<HTMLImageElement>) => {
     if (manInputRef.current) {
       manInputRef.current.click();
       setManCheck(true);
       setWomanCheck(false);
-      setGender('MALE')
+      setGender('MALE');
     }
   };
 
   // 여자 아이콘
-  const womanUrl = process.env.PUBLIC_URL + "/assets/woman.png";
+  const womanUrl = process.env.PUBLIC_URL + '/assets/woman.png';
   const [womanCheck, setWomanCheck] = useState<boolean>(false);
   const womanInputRef = useRef<HTMLInputElement | null>(null);
-  const womanId = womanCheck ? "womanCheck" : "womanUncheck";
+  const womanId = womanCheck ? 'womanCheck' : 'womanUncheck';
   const handleWomanIconClick = (event: MouseEvent<HTMLImageElement>) => {
     if (womanInputRef.current) {
       womanInputRef.current.click();
       setWomanCheck(true);
       setManCheck(false);
-      setGender("FEMALE");
+      setGender('FEMALE');
     }
   };
   return (
     <Form>
       <span>
-        <input type="email" placeholder="아이디(이메일 형식)" value={email} onChange={onChangeEmail} />
+        <input
+          type="email"
+          placeholder="아이디(이메일 형식)"
+          value={email}
+          onChange={onChangeEmail}
+        />
         <button id="check">중복확인</button>
       </span>
       <span id="nickname">
-        <input type="text" placeholder="닉네임" value={nickName} onChange={onChangeNickName} />
+        <input
+          type="text"
+          placeholder="닉네임"
+          value={nickName}
+          onChange={onChangeNickName}
+        />
         <button id="check">중복확인</button>
       </span>
       <span>
-        <input type="password" placeholder="비밀번호" value={pwd} onChange={onChangePwd} />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          value={pwd}
+          onChange={onChangePwd}
+        />
       </span>
       <span>
-        <input type="password" placeholder="비밀번호 확인" value={pwdc} onChange={onChangePwdc} />
+        <input
+          type="password"
+          placeholder="비밀번호 확인"
+          value={pwdc}
+          onChange={onChangePwdc}
+        />
       </span>
       <span>
         <input type="date" id="date" value={birth} onChange={onChangeBirth} />
@@ -95,7 +125,9 @@ const SignUpForm = () => {
         <input type="checkbox" id="woman" ref={womanInputRef} />
       </GenderWrapper>
 
-      <SubmitButton type="submit" onClick={signUp}>회 원 가 입</SubmitButton>
+      <SubmitButton type="submit" onClick={signUp}>
+        회 원 가 입
+      </SubmitButton>
     </Form>
   );
 };

26c82d6c77b3d27f193bf2aadfce55a966b44274
이서현 <kathyleesh7@gmail.com>
2023-09-21 13:08:43 +0900

    chore(EmailConfig) : #133 - email log
    email log 추가
    
    Related to: #133

--- backend/authentication-service/src/main/java/com/authenticationservice/global/config/EmailConfig.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/global/config/EmailConfig.java
@@ -2,21 +2,20 @@ package com.authenticationservice.global.config;
 
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+@Slf4j
 @Getter
 @Configuration
 public class EmailConfig {
 
     @Value("${spring.mail.host}")
     private String host;
-
-    @Value("${spring.mail.port}")
-    private int port;
 
     @Value("${spring.mail.email-expiration-time}")
     private String emailExpirationTime;
@@ -30,9 +29,10 @@ public class EmailConfig {
 
     @Bean
     public JavaMailSender javaMailSender() {
+        log.info("host : {}, username: {}, password: {}", host, address, password);
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost(host); // SMTP 서버 호스트
-        mailSender.setPort(port); // SMTP 서버 포트
+        mailSender.setPort(587); // SMTP 서버 포트
         mailSender.setUsername(address); // 이메일 계정
         mailSender.setPassword(password); // 이메일 비밀번호
 

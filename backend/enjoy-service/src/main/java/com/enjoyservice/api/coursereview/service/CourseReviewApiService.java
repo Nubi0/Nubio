adf8ffcd8297e49dff3aa696d59a8ad7a5452ddf
김윤욱 <reznov03048@gmail.com>
2023-09-21 12:50:39 +0900

    feat(CourseReviewApiService) : #135 - CourseReview 생성 기능 구현
    CourseReview 생성 기능 구현
    
    Related to: #135

---
+++ backend/enjoy-service/src/main/java/com/enjoyservice/api/coursereview/service/CourseReviewApiService.java
@@ -0,0 +1,8 @@
+package com.enjoyservice.api.coursereview.service;
+
+import com.enjoyservice.api.coursereview.dto.CourseReviewCreate;
+
+public interface CourseReviewApiService {
+
+    CourseReviewCreate.Res createCourseReview(Long courseId, CourseReviewCreate.Req request, String memberId);
+}

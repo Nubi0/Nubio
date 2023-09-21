7e9718c446cbd040ff56f233cf3e77a81e765cc4
이서현 <kathyleesh7@gmail.com>
2023-09-21 10:06:46 +0900

    feat(Email) : 이메일 인증 확인 요청 및 예외 처리
    - 이메일과 인증코드를 받아 인증 코드 확인
    - 맞지 않을 경우 예외 처리
    
    Related to: #134

--- backend/authentication-service/src/main/java/com/authenticationservice/external/email/dto/request/EmailConfirmDto.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/external/email/dto/request/EmailConfirmDto.java
@@ -2,11 +2,10 @@ package com.authenticationservice.external.email.dto.request;
 
 import jakarta.validation.constraints.Email;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
-public class EmailReqDto {
+public class EmailConfirmDto {
     @Email
     private String email;
+    private String code;
 }

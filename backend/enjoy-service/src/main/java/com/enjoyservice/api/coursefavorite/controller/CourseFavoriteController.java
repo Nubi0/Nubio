9f23cc2a2926d474c284449998b2a62f34bf5c14
김윤욱 <reznov03048@gmail.com>
2023-09-22 15:25:19 +0900

    feat(CourseFavoriteController) : #156 - 코스 즐겨찾기/즐겨찾기 취소 controller 구현
    코스 즐겨찾기/즐겨찾기 취소 controller 구현
    
    Related to: #156

---
+++ backend/enjoy-service/src/main/java/com/enjoyservice/api/coursefavorite/controller/CourseFavoriteController.java
@@ -0,0 +1,27 @@
+package com.enjoyservice.api.coursefavorite.controller;
+
+import com.enjoyservice.api.ApiResponse;
+import com.enjoyservice.api.coursefavorite.dto.CourseFavoriteRes;
+import com.enjoyservice.api.coursefavorite.service.CourseFavoriteApiService;
+import com.enjoyservice.global.resolver.memberinfo.MemberInfo;
+import com.enjoyservice.global.resolver.memberinfo.MemberInfoDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/enjoy/course")
+@RequiredArgsConstructor
+public class CourseFavoriteController {
+
+    private final CourseFavoriteApiService courseFavoriteApiService;
+
+    @PostMapping("/favorite/{courseId}")
+    public ApiResponse<CourseFavoriteRes> courseFavorite(@PathVariable("courseId") Long courseId,
+                                                         @MemberInfo MemberInfoDto memberInfoDto) {
+        CourseFavoriteRes response = courseFavoriteApiService.courseFavorite(courseId, memberInfoDto.getMemberId());
+        return ApiResponse.ok(response);
+    }
+}

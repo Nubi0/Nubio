4b10ac197aa516acc645dc728a90943b3d548a57
jyj143 <jyj1143@gmail.com>
2023-09-22 10:39:35 +0900

    feat(CourseTageListRes) : #151 - Create CourseTageListRes
     - Create CourseTageListRes
    
    Related to: #151

--- backend/enjoy-service/src/main/java/com/enjoyservice/api/course/dto/CourseTagListReq.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/api/course/dto/CourseTagListReq.java
@@ -1,2 +1,16 @@
-package com.enjoyservice.api.course.dto;public class CourseTagListRes {
+package com.enjoyservice.api.course.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+public class CourseTagListReq {
+    @JsonProperty("course_tags")
+    private List<String> course_tags;
 }

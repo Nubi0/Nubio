de52f010d99117093821fa6960b99336d4742573
김윤욱 <reznov03048@gmail.com>
2023-09-22 17:12:47 +0900

    feat(CourseController) : #166 - 코스 관련 swagger
    코스 관련 swagger
    
    Related to: #166

--- backend/enjoy-service/src/main/java/com/enjoyservice/api/course/controller/CourseController.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/api/course/controller/CourseController.java
@@ -9,6 +9,7 @@ import com.enjoyservice.api.course.service.CourseApiService;
 import com.enjoyservice.global.resolver.memberinfo.MemberInfo;
 import com.enjoyservice.global.resolver.memberinfo.MemberInfoDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -27,20 +28,24 @@ public class CourseController {
 
     private final CourseApiService courseApiService;
 
-    @Operation(summary = "코스 생성", description = "api/v1/enjoy/course\n\n" )
+    @Operation(summary = "코스 생성", description = "enjoy/v1/enjoy/course\n\n" )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description =  "CREATED"),
     })
     @PostMapping("/course")
     public ApiResponseEntity<String> createCourse(@Valid @RequestBody CourseCreateReq courseCreateReq,
-                                                  @MemberInfo MemberInfoDto memberInfoDto) {
+                                                  @MemberInfo @Parameter(hidden = true) MemberInfoDto memberInfoDto) {
         courseApiService.createCourse(courseCreateReq, memberInfoDto.getMemberId());
         return ApiResponseEntity.created("생성 성공");
     }
 
+    @Operation(summary = "코스 목록 조회(지역별)", description = "enjoy/v1/enjoy/course\n\n" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description =  "OK"),
+    })
     @GetMapping("/course")
     public ApiResponseEntity<CourseListRes> getCourseList(@RequestParam("region") String region,
-                                                          @MemberInfo MemberInfoDto memberInfoDto,
+                                                          @MemberInfo @Parameter(hidden = true) MemberInfoDto memberInfoDto,
                                                           @PageableDefault(size = 100,
                                                             sort = "createTime",
                                                             direction = Sort.Direction.DESC) Pageable pageable) {
@@ -48,15 +53,24 @@ public class CourseController {
         return ApiResponseEntity.ok(response);
     }
 
+    @Operation(summary = "코스 상세 조회", description = "enjoy/v1/enjoy/course/{courseId}\n\n" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description =  "OK"),
+    })
     @GetMapping("/course/{courseId}")
-    public ApiResponseEntity<CourseDetailRes> getCourseDetail(@PathVariable Long courseId, @MemberInfo MemberInfoDto memberInfoDto) {
+    public ApiResponseEntity<CourseDetailRes> getCourseDetail(@PathVariable Long courseId,
+                                                              @MemberInfo @Parameter(hidden = true) MemberInfoDto memberInfoDto) {
         CourseDetailRes response = courseApiService.getCourseDetail(courseId, memberInfoDto.getMemberId());
         return ApiResponseEntity.ok(response);
     }
 
+    @Operation(summary = "태그 리스트로 코스 목록 조회", description = "enjoy/v1/enjoy/course/filter\n\n" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description =  "OK"),
+    })
     @PostMapping("/course/filter")
     public ApiResponseEntity<CourseListRes> getCourseDetailByCourseTags(@RequestBody CourseTagListReq courseTagListReq,
-                                                                        @MemberInfo MemberInfoDto memberInfoDto,
+                                                                        @MemberInfo @Parameter(hidden = true) MemberInfoDto memberInfoDto,
                                                                         @PageableDefault(size = 100,
                                                             sort = "createTime",
                                                             direction = Sort.Direction.DESC) Pageable pageable) {

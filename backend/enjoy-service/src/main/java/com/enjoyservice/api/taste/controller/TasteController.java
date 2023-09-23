a8492b891e930433c5a721e19e08e93742a348b9
김윤욱 <reznov03048@gmail.com>
2023-09-23 20:54:54 +0900

    feat(TasteController) : #166 - 취향 swagger
    취향 swagger
    
    Related to: #166

--- backend/enjoy-service/src/main/java/com/enjoyservice/api/taste/controller/TasteController.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/api/taste/controller/TasteController.java
@@ -6,9 +6,15 @@ import com.enjoyservice.api.taste.dto.update.MemberTasteReq;
 import com.enjoyservice.api.taste.service.TasteApiService;
 import com.enjoyservice.global.resolver.memberinfo.MemberInfo;
 import com.enjoyservice.global.resolver.memberinfo.MemberInfoDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Taste API", description = "취향 api")
 @RestController
 @RequestMapping("/v1/enjoy/profile")
 @RequiredArgsConstructor
@@ -16,21 +22,33 @@ public class TasteController {
 
     private final TasteApiService tasteApiService;
 
+    @Operation(summary = "취향 조회", description = "enjoy/v1/enjoy/profile/taste\n\n" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description =  "OK"),
+    })
     @GetMapping("/taste")
-    public ApiResponseEntity<TasteApiRes> searchTaste(@MemberInfo MemberInfoDto memberInfoDto) {
+    public ApiResponseEntity<TasteApiRes> searchTaste(@MemberInfo @Parameter(hidden = true) MemberInfoDto memberInfoDto) {
         TasteApiRes tasteApiRes = tasteApiService.searchTaste(memberInfoDto.getMemberId());
         return ApiResponseEntity.ok(tasteApiRes);
     }
 
+    @Operation(summary = "취향 수정", description = "enjoy/v1/enjoy/profile/taste\n\n" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description =  "OK"),
+    })
     @PutMapping("/taste")
-    public ApiResponseEntity<String> updateTaste(@MemberInfo MemberInfoDto memberInfoDto,
+    public ApiResponseEntity<String> updateTaste(@MemberInfo @Parameter(hidden = true) MemberInfoDto memberInfoDto,
                                                  @RequestBody MemberTasteReq memberTasteReq) {
         tasteApiService.updateTaste(memberInfoDto.getMemberId(), memberTasteReq);
         return ApiResponseEntity.ok("수정 완료 하였습니다.");
     }
 
+    @Operation(summary = "취향 생성", description = "enjoy/v1/enjoy/profile/taste\n\n" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description =  "OK"),
+    })
     @PostMapping("/taste")
-    public ApiResponseEntity<String> createTaste(@MemberInfo MemberInfoDto memberInfoDto,
+    public ApiResponseEntity<String> createTaste(@MemberInfo @Parameter(hidden = true) MemberInfoDto memberInfoDto,
                                                  @RequestBody MemberTasteReq memberTasteReq) {
         tasteApiService.createTaste(memberInfoDto.getMemberId(), memberTasteReq);
         return ApiResponseEntity.ok("생성 완료 하였습니다.");

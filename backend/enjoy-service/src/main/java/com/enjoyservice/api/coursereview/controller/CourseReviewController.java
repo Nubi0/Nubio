77b91e0699285bcbbfca4f2efa3f8eefe02cfa5e
김윤욱 <reznov03048@gmail.com>
2023-09-23 20:48:15 +0900

    feat(CourseReviewController) : #166 - 코스 리뷰 swagger
    코스 리뷰 swagger
    
    Related to: #166

--- backend/enjoy-service/src/main/java/com/enjoyservice/api/coursereview/controller/CourseReviewController.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/api/coursereview/controller/CourseReviewController.java
@@ -6,6 +6,11 @@ import com.enjoyservice.api.coursereview.dto.CourseReviewInfosRes;
 import com.enjoyservice.api.coursereview.service.CourseReviewApiService;
 import com.enjoyservice.global.resolver.memberinfo.MemberInfo;
 import com.enjoyservice.global.resolver.memberinfo.MemberInfoDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -13,13 +18,18 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "CourseReview API", description = "코스 리뷰 api")
 @RestController
-@RequestMapping("api/v1/enjoy/course")
+@RequestMapping("v1/enjoy/course")
 @RequiredArgsConstructor
 public class CourseReviewController {
 
     private final CourseReviewApiService courseReviewApiService;
 
+    @Operation(summary = "코스 리뷰 조회", description = "enjoy/v1/enjoy/course/review/{courseId}\n\n" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description =  "CREATED"),
+    })
     @GetMapping("/review/{courseId}")
     public ApiResponseEntity<CourseReviewInfosRes> getCourseReviews(@PathVariable("courseId") Long courseId,
                                                                     @PageableDefault(size = 100,
@@ -29,14 +39,22 @@ public class CourseReviewController {
         return ApiResponseEntity.ok(response);
     }
 
+    @Operation(summary = "코스 리뷰 생성", description = "enjoy/v1/enjoy/course/review/{courseId}\n\n" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description =  "OK"),
+    })
     @PostMapping("/review/{courseId}")
     public ApiResponseEntity<CourseReviewCreate.Res> createCourseReview(@PathVariable("courseId") Long courseId,
                                                                         @Valid @RequestBody CourseReviewCreate.Req request,
-                                                                        @MemberInfo MemberInfoDto memberInfoDto) {
+                                                                        @MemberInfo @Parameter(hidden = true) MemberInfoDto memberInfoDto) {
         CourseReviewCreate.Res response = courseReviewApiService.createCourseReview(courseId, request, memberInfoDto.getMemberId());
         return ApiResponseEntity.created(response);
     }
 
+    @Operation(summary = "코스 리뷰 삭제", description = "enjoy/v1/enjoy/course/review/{courseReviewId}\n\n" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+    })
     @DeleteMapping("/review/{courseReviewId}")
     public ApiResponseEntity<String> deleteCourseReview(@PathVariable("courseReviewId") Long courseReviewId) {
         courseReviewApiService.deleteCourseReview(courseReviewId);

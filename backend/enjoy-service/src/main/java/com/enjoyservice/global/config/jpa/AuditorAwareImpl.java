1c4d9435b99a312f43f641df97889e95f4b7c350
이서현 <kathyleesh7@gmail.com>
2023-09-23 13:03:22 +0900

    fix(RequiredArgsConstructor) : #167 - 의존성 주입
    의존성 주입으로 변경
    
    Related to: #167

--- backend/enjoy-service/src/main/java/com/enjoyservice/global/config/jpa/AuditorAwareImpl.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/global/config/jpa/AuditorAwareImpl.java
@@ -2,15 +2,17 @@ package com.enjoyservice.global.config.jpa;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.util.StringUtils;
 
 import java.util.Optional;
 
-@RequiredArgsConstructor
+
 public class AuditorAwareImpl implements AuditorAware<String> {
 
-    private final HttpServletRequest httpServletRequest;
+    @Autowired
+    private HttpServletRequest httpServletRequest;
 
     @Override
     public Optional<String> getCurrentAuditor() {

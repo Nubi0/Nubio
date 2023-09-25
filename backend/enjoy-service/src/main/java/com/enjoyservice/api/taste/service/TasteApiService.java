8c5528288c633c4a8037dd1f68c023af465e5d90
김민규 <melon212@naver.com>
2023-09-25 13:51:49 +0900

    feat(TasteApiService) : #210- Taste 생성
    - Taste entity 저장
    
    Related to: #210

--- backend/enjoy-service/src/main/java/com/enjoyservice/api/taste/service/TasteApiService.java
+++ backend/enjoy-service/src/main/java/com/enjoyservice/api/taste/service/TasteApiService.java
@@ -1,5 +1,6 @@
 package com.enjoyservice.api.taste.service;
 
+import com.enjoyservice.api.taste.dto.create.TasteInfoReq;
 import com.enjoyservice.api.taste.dto.search.TasteApiRes;
 import com.enjoyservice.api.taste.dto.update.MemberTasteReq;
 
@@ -7,4 +8,5 @@ public interface TasteApiService {
     TasteApiRes searchTaste(String memberId);
     void updateTaste(String memberId, MemberTasteReq memberTasteReq);
     void createTaste(String memberId, MemberTasteReq memberTasteReq);
+    void saveTaste(TasteInfoReq tasteInfoReq);
 }

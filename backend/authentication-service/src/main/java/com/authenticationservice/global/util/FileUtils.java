030eddcef9db07fd814f39a450bc56753ee58b15
이서현 <kathyleesh7@gmail.com>
2023-09-25 09:42:24 +0900

    feat(FileUtils) : #147 - file name build
    파일 이름 지정 util
    
    Related to: #147

---
+++ backend/authentication-service/src/main/java/com/authenticationservice/global/util/FileUtils.java
@@ -0,0 +1,21 @@
+package com.authenticationservice.global.util;
+
+import java.util.UUID;
+
+public class FileUtils {
+
+    private static final String FILE_EXTENSION_SEPARATOR = ".";
+    private static final String CATEGORY_PREFIX = "/";
+    private static final String NAME_SEPARATOR = "_";
+
+    public static String buildFileName(String category, String originalFileName, UUID uuid) {
+        // 파일 확장자
+        int fileExtensionIndex = originalFileName.lastIndexOf(FILE_EXTENSION_SEPARATOR);
+        String fileExtension = originalFileName.substring(fileExtensionIndex);
+        // 파일 이름
+        String fileName = originalFileName.substring(0, fileExtensionIndex);
+
+        return category +  CATEGORY_PREFIX + fileName + NAME_SEPARATOR + uuid + fileExtension;
+    }
+
+}

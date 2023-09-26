e9f0fabf8916cbe32018fbd0ebc4450172a8f12c
김윤욱 <reznov03048@gmail.com>
2023-09-26 09:35:35 +0900

    feat(ErrorCode) : #227 - refresh-token 관련 에러 추가
    - NOT_EXISTS_AUTHORIZATION(HttpStatus.UNAUTHORIZED, "A-004", "Authorization Header가 빈값입니다.")
    - NOT_VALID_BEARER_GRANT_TYPE(HttpStatus.UNAUTHORIZED, "A-005", "인증 타입이 Bearer 타입이 아닙니다.")
    - REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "A-006", "해당 refresh token은 존재하지 않습니다.")
    - REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "A-007", "해당 refresh token은 만료됐습니다.")
    
    Related to: #227

--- backend/authentication-service/src/main/java/com/authenticationservice/global/error/ErrorCode.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/global/error/ErrorCode.java
@@ -21,6 +21,10 @@ public enum ErrorCode {
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "A-001", "토큰이 만료되었습니다."),
     NOT_VALID_TOKEN(HttpStatus.UNAUTHORIZED, "A-002", "해당 토큰은 유효한 토큰이 아닙니다."),
     EMAIL_CONFIRM_FAILED(HttpStatus.BAD_REQUEST, "A-003", "이메일 인증 실패하였습니다."),
+    NOT_EXISTS_AUTHORIZATION(HttpStatus.UNAUTHORIZED, "A-004", "Authorization Header가 빈값입니다."),
+    NOT_VALID_BEARER_GRANT_TYPE(HttpStatus.UNAUTHORIZED, "A-005", "인증 타입이 Bearer 타입이 아닙니다."),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "A-006", "해당 refresh token은 존재하지 않습니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "A-007", "해당 refresh token은 만료됐습니다."),
 
     // 프로필
     PROFILE_SIZE_LIMIT(HttpStatus.BAD_REQUEST, "P-003", "제출할 수 있는 파일 사이즈를 초과했습니다."),

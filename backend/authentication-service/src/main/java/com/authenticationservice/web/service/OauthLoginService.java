53476944c9d1b434e5f5df603b41ac19400c4089
김윤욱 <reznov03048@gmail.com>
2023-10-04 14:17:13 +0900

    feat(member) : #299 - kakao 회원가입 시 password 임의 삽입
    kakao 회원가입 시 password 임의 삽입
    
    Related to: #299

--- backend/authentication-service/src/main/java/com/authenticationservice/web/service/OauthLoginService.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/web/service/OauthLoginService.java
@@ -4,6 +4,7 @@ import com.authenticationservice.domain.member.entity.Member;
 import com.authenticationservice.domain.member.entity.constant.OAuthType;
 import com.authenticationservice.domain.member.entity.constant.Role;
 import com.authenticationservice.domain.member.entity.type.Email;
+import com.authenticationservice.domain.member.entity.type.Password;
 import com.authenticationservice.domain.member.service.MemberService;
 import com.authenticationservice.external.oauth.model.OAuthAttributes;
 import com.authenticationservice.external.oauth.service.SocialLoginApiService;
@@ -13,6 +14,7 @@ import com.authenticationservice.global.jwt.service.JwtManager;
 import com.authenticationservice.web.dto.OauthLoginResDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +28,7 @@ public class OauthLoginService {
 
     private final MemberService memberService;
     private final JwtManager jwtManager;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public OauthLoginResDto oauthLogin(String accessToken, OAuthType oauthType) {
@@ -39,6 +42,7 @@ public class OauthLoginService {
         if(optionalMember.isEmpty()) { // 신규 회원 가입
             log.info("kakao 신규 유저 회원가입");
             oauthMember = userInfo.toMemberEntity(oauthType, Role.ROLE_USER);
+            oauthMember.setPassword(Password.of("random", passwordEncoder));
             oauthMember = memberService.register(oauthMember);
         } else { // 기존 회원일 경우
             log.info("kakao 기존 유저 로그인");

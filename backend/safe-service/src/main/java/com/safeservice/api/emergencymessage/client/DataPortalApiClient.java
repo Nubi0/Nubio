9ad1d38b6bcf4e248c19445b44ec94e87c39f59a
김민규 <melon212@naver.com>
2023-10-17 15:39:33 +0900

    feat(SchedulerApiClient) : #362 - 공공데이터 SchedulerApiClient 구현
    - 공공데이터 SchedulerApiClient 구현
    
    Related to: #362

--- backend/safe-service/src/main/java/com/safeservice/api/emergencymessage/client/DataPortalApiClient.java
+++ backend/safe-service/src/main/java/com/safeservice/api/emergencymessage/client/DataPortalApiClient.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(url = "https://apis.data.go.kr/1741000/DisasterMsg3/getDisasterMsg1List", name = "DataPortalClient")
 public interface DataPortalApiClient {
-    String serviceKey = "ZBlbsX27bnVykE6yaeVoKS00GpfGJkqMm%2BEVd8dDP9D8w2rK8bfe1PzwM7z%2B3XskXJM2eYo4PYVC6D374zGtjQ%3D%3D";
     @GetMapping(consumes = "application/json")
     String requestDataApi(@RequestParam("serviceKey") String serviceKey,
                           @RequestParam("pageNo") String pageNo,

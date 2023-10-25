6051f6b7d85d0f63f844cebe15636572870df43f
김민규 <melon212@naver.com>
2023-10-25 15:21:23 +0900

    feat(ReportService) : #386 - Report 조회 구현
    - Report 조회 구현
    
    Related to: #386

--- backend/safe-service/src/main/java/com/safeservice/domain/report/entity/type/report/Allow.java
+++ backend/safe-service/src/main/java/com/safeservice/domain/report/entity/type/report/Allow.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Allow {
-    @Column(name = "active")
+    @Column(name = "allow")
     private boolean value;
 
     private Allow(final boolean value) {

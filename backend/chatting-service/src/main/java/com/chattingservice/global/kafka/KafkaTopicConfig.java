76c11f24472c6c0d71d57bdd7edac6054744e709
jyj143 <jyj1143@gmail.com>
2023-10-26 07:53:55 +0900

    feat(KafkaTopicConfig) : #379 - 채팅메시지, 룸메시지을 위한 NewTopic을 2개 생성
    - 채팅메시지, 룸메시지을 위한 NewTopic을 2개 생성
    
    Related to: #379

---
+++ backend/chatting-service/src/main/java/com/chattingservice/global/kafka/KafkaTopicConfig.java
@@ -0,0 +1,48 @@
+package com.chattingservice.global.kafka;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+    @Value("${kafka.topic.chat-name}")
+    private String topicChatName;
+    @Value("${kafka.topic.room-name}")
+    private String topicRoomName;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin(){
+        Map<String,Object> configurations = new HashMap<>();
+        configurations.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapAddress);
+        configurations.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, 60000); // 60 seconds
+        configurations.put(AdminClientConfig.RETRY_BACKOFF_MS_CONFIG, 120000); // 120 seconds
+        return new KafkaAdmin(configurations);
+    }
+
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name(topicChatName)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+    @Bean
+    public NewTopic roomTopic() {
+        return TopicBuilder.name(topicRoomName)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+}

8ffe1ef0170bdec84922129155e6cad3e0794d15
jyj143 <jyj1143@gmail.com>
2023-10-26 07:38:42 +0900

    feat(RespRoomDto) : #379 - 방생성 DTO
     - 방생성 DTO
    
    Related to: #379

---
+++ backend/chatting-service/src/main/java/com/chattingservice/global/kafka/dto/request/RespRoomDto.java
@@ -0,0 +1,26 @@
+package com.chattingservice.global.kafka.dto.request;
+
+import com.chattingservice.domain.room.entity.Member;
+import com.chattingservice.global.kafka.dto.RoomType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Schema(description = "방생성 정보")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class RespRoomDto {
+    private String room_id;
+    private String title;
+    @Schema(description = "1:1 - DM, 그룹 - GROUP")
+    private RoomType type;
+    private List<Member> members;
+    private List<String> managers;
+    private LocalDateTime createdAt;
+}

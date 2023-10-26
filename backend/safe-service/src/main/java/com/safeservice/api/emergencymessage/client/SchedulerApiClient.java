1bc11a2e0da78a15c3a437b9208e2be1b3d16b3e
김민규 <melon212@naver.com>
2023-10-26 16:27:42 +0900

    fix(SchedulerApiClientImpl) : #408 - api url 수정
     - api url 수정
    
    Related to: #408

--- backend/safe-service/src/main/java/com/safeservice/api/emergencymessage/client/SchedulerApiClient.java
+++ backend/safe-service/src/main/java/com/safeservice/api/emergencymessage/client/SchedulerApiClient.java
@@ -3,7 +3,7 @@ package com.safeservice.api.emergencymessage.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(url = "https://nubi0.com/api/safe/v1/safe", name = "schedulerClient")
+@FeignClient(url = "http://ec2-13-124-94-38.ap-northeast-2.compute.amazonaws.com:8082/v1/safe", name = "schedulerClient")
 public interface SchedulerApiClient {
 
     @GetMapping(value = "/emergency/call")

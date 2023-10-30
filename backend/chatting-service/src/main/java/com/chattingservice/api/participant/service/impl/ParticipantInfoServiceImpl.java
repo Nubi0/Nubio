20ba5fe27129b686ae316a70059408a01ee22a22
jyj143 <jyj1143@gmail.com>
2023-10-31 08:46:48 +0900

    feat(ParticipantInfoServieImpl) : #379 - 마지막 읽은 메시지 수정 구현
     - 마지막 읽은 메시지 수정 구현
    
    Related to: #379

--- backend/chatting-service/src/main/java/com/chattingservice/api/participant/service/impl/ParticipantInfoServiceImpl.java
+++ backend/chatting-service/src/main/java/com/chattingservice/api/participant/service/impl/ParticipantInfoServiceImpl.java
@@ -2,34 +2,71 @@ package com.chattingservice.api.participant.service.impl;
 
 import com.chattingservice.api.participant.dto.response.ParticipantRes;
 import com.chattingservice.api.participant.service.ParticipantInfoService;
-import com.chattingservice.global.resolver.memberInfo.MemberInfoDto;
+import com.chattingservice.api.profile.service.ProfileUploadService;
+import com.chattingservice.domain.chattingroom.entity.ChattingRoom;
+import com.chattingservice.domain.chattingroom.repository.ChattingRoomRepository;
+import com.chattingservice.domain.participant.ParticipantService;
+import com.chattingservice.domain.participant.enity.Participant;
+import com.chattingservice.domain.participant.repository.ParticipantRepository;
+import com.chattingservice.domain.profile.service.ProfileService;
+import com.chattingservice.global.error.ErrorCode;
+import com.chattingservice.global.error.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Optional;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class ParticipantInfoServiceImpl implements ParticipantInfoService {
 
+    private final ParticipantService participantService;
+    private final ChattingRoomRepository chattingRoomRepository;
+    private final ProfileUploadService profileUploadService;
 
+    @Transactional
     @Override
-    public ParticipantRes register(MemberInfoDto memberInfo, Long roomId, MultipartFile profileImg, String nickName) {
-        return null;
+    public ParticipantRes updateMemberInfo(String memberId, Long roomId, MultipartFile profileImg, String nickName) {
+
+        Participant participant = validateParticipate(memberId, roomId);
+
+        if (profileImg != null) {
+            profileUploadService.uploadProfile("participant",participant,profileImg);
+        }
+        if(nickName!=null) participant.getNickname().setValue(nickName);
+
+        return ParticipantRes.of(participant);
     }
 
+    @Transactional
     @Override
-    public ParticipantRes updateMemberInfo(MemberInfoDto memberInfo, Long roomId, MultipartFile profileImg, String nickName) {
-        return null;
+    public ParticipantRes updateLastReadMsgId(String memberId, Long roomId, String lastReadMsg) {
+        Participant participant = validateParticipate(memberId, roomId);
+        participant.setLastReadMsgId(lastReadMsg);
+
+        return ParticipantRes.of(participant);
     }
 
-    @Override
-    public ParticipantRes updateLastReadMsgId(String lastReadMsg) {
-        return null;
+    private Participant validateParticipate(String memberId, Long roomId) {
+        ChattingRoom chattingRoom = chattingRoomRepository.findById(roomId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ROOM_NOT_EXIST));
+        Participant participant = participantService.findByMemberIdAndChattingRoom(memberId, chattingRoom).orElseThrow(
+                () -> new BusinessException(ErrorCode.PARTICIPANT_NOT_EXIST)
+        );
+        return participant;
     }
 
-    @Override
-    public void deleteParticipant(String memberId, Long roomId) {
-
-    }
 
     @Override
     public ParticipantRes findByMemberId(String memberId, Long roomId) {
-        return null;
+
+        Participant participant = validateParticipate(memberId, roomId);
+
+        return ParticipantRes.of(participant);
     }
 }

0fd0411db7030604e822f862f279308eb51db836
jyj143 <jyj1143@gmail.com>
2023-10-30 17:20:21 +0900

    feat(ChattingRoomController) : #379 - ChattingRoomController생성
    - ChattingRoomController생성
    
    Related to: #379

---
+++ backend/chatting-service/src/main/java/com/chattingservice/api/chattingroom/controller/ChattingRoomController.java
@@ -0,0 +1,43 @@
+package com.chattingservice.api.chattingroom.controller;
+
+import com.chattingservice.api.ApiResponseEntity;
+import com.chattingservice.api.chattingroom.dto.request.ChattingRoomEnterReq;
+import com.chattingservice.api.chattingroom.dto.request.ChattingRoomOutReq;
+import com.chattingservice.api.chattingroom.dto.response.ChattingRoomResp;
+import com.chattingservice.api.chattingroom.service.ChattingRoomInfoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "ChattingRoom API", description = "채팅방 api")
+@RestController
+@RequestMapping("/v1/chatting/room")
+@RequiredArgsConstructor
+public class ChattingRoomController {
+
+    private final ChattingRoomInfoService chattingRoomInfoService;
+
+    @Operation(summary = "그룹 채팅방 입장", description = "chatting/v1/chatting/room/enter\n\n")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
+    @PostMapping("/enter")
+    public ApiResponseEntity<ChattingRoomResp> enterGroupRoom(@RequestBody ChattingRoomEnterReq chattingRoomEnterReq) {
+        ChattingRoomResp chattingRoomResp = chattingRoomInfoService.enterGroupRoom(chattingRoomEnterReq);
+        return ApiResponseEntity.ok(chattingRoomResp);
+    }
+
+    @Operation(summary = "그룹 채팅방 나가기", description = "chatting/v1/chatting/room/out\n\n")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
+    @PostMapping("/out")
+    public ApiResponseEntity<ChattingRoomResp> outOfGroupRoom(@RequestBody ChattingRoomOutReq chattingRoomOutReq) {
+        ChattingRoomResp chattingRoomResp = chattingRoomInfoService.outOfGroupRoom(chattingRoomOutReq);
+        return ApiResponseEntity.ok(chattingRoomResp);
+    }
+
+}

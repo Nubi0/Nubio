217ccf6e8352fba1da245ace3e7e49a5839dfc1e
jyj143 <jyj1143@gmail.com>
2023-10-31 09:03:48 +0900

    feat(room) : #379 - room패키지 설정
    - room패키지 설정
    
    Related to: #379

---
+++ backend/chatting-service/src/main/java/com/chattingservice/domain/room/dto/request/ReqReadMessage.java
@@ -0,0 +1,14 @@
+package com.chattingservice.domain.room.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReqReadMessage {
+    private String room_id;
+    private String user_id;
+    private String message_id;
+}

27cc217a37eec92e0b70b6477a3e1bf0579f0b22
jyj143 <jyj1143@gmail.com>
2023-10-31 11:41:35 +0900

    feat(SwaggerConfig) : #379 - 스웨거JWT bearer 헤더 생성
    - 스웨거JWT bearer 헤더 생성
    
    Related to: #379

--- backend/chatting-service/src/main/java/com/chattingservice/global/swagger/SwaggerConfig.java
+++ backend/chatting-service/src/main/java/com/chattingservice/global/swagger/SwaggerConfig.java
@@ -1,21 +1,36 @@
 package com.chattingservice.global.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
-                .info(new Info().title("누비오-CHATTING-API Document")
-                        .description("누비오 채팅 서비스 API 명세입니다.")
-                        .version("1.0.0"));
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name("JWT")
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT"))
+                )
+                .info(new Info()
+                        .title("누비오-CHATTING-API Document")     /* 서비스 제목 */
+                        .version("1.0.0")   /* 서비스 버전 */
+                        .description("누비오 채팅 서비스 API 명세입니다.\n"));    /* 서비스 설명 */
     }
+
 
 }

993699a29655626e166003b68bde788ecf4b26eb
jyj143 <jyj1143@gmail.com>
2023-11-02 10:53:55 +0900

    feat(ChattingRoomEnterWithProfileResp) : #379 -프로필 입력해서 채팅방 입력DTO생성
    -프로필 입력해서 채팅방 입력DTO생성
    
    Related to: #379

--- backend/chatting-service/src/main/java/com/chattingservice/api/chatting/dto/request/ChatMessagePageDto.java
+++ backend/chatting-service/src/main/java/com/chattingservice/api/chatting/dto/request/ChatMessagePageDto.java
@@ -25,6 +25,40 @@ public class ChatMessagePageDto {
     @JsonProperty("meta")
     private PageMetaDto meta;
 
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class PageMetaDto {
+
+        @JsonProperty("page_number")
+        private int pageNumber;
+
+        @JsonProperty("page_size")
+        private int pageSize;
+
+        @JsonProperty("total_elements")
+        private long totalElements;
+
+        @JsonProperty("total_pages")
+        private int totalPages;
+
+        @JsonProperty("last_flag")
+        private boolean lastFlag;
+
+        static public PageMetaDto from(Page<MessageCollection> chatMessageDtos) {
+            return PageMetaDto.builder()
+                    .pageNumber(chatMessageDtos.getNumber())
+                    .pageSize(chatMessageDtos.getSize())
+                    .totalElements(chatMessageDtos.getTotalElements())
+                    .totalPages(chatMessageDtos.getTotalPages())
+                    .lastFlag(chatMessageDtos.isLast())
+                    .build();
+        }
+
+
+    }
+
     static public ChatMessagePageDto from(Page<MessageCollection> chatMessageDtos) {
         return ChatMessagePageDto.builder()
                 .meta(PageMetaDto.from(chatMessageDtos))

ca8737089934a7ce7b69a3f162f355718b025827
김민규 <melon212@naver.com>
2023-11-10 17:19:21 +0900

    feat(KakaoPlaceService) : #480 - 엘라스틱서치 service 생성
    - 엘라스틱서치 service 생성
    
    Related to: #480

---
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/KakaoPlace/service/KakaoPlaceService.java
@@ -0,0 +1,4 @@
+package com.enjoyservice.domain.KakaoPlace.service;
+
+public interface KakaoPlaceService {
+}
---
+++ backend/enjoy-service/src/main/java/com/enjoyservice/domain/KakaoPlace/service/impl/KakaoPlaceServiceImpl.java
@@ -0,0 +1,10 @@
+package com.enjoyservice.domain.KakaoPlace.service.impl;
+
+import com.enjoyservice.domain.KakaoPlace.service.KakaoPlaceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoPlaceServiceImpl implements KakaoPlaceService {
+}
